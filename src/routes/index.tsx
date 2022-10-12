1c8d4937955e9db980e206e8b521d7ddb7d0d387
Srujan Gurram <srujangs8@gmail.com>
2022-10-12 23:32:40 +0530

    added tailwind to project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <h1>Hello world 🚀</h1>;
+  return <h1 class="underline">Hello world 🚀</h1>;
 });
 
 export const head: DocumentHead = {
