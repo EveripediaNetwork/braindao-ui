bbf1a77278afdf07fa776c9e525d341b4dd8f44d
Srujan Gurram <srujangs8@gmail.com>
2022-10-14 07:52:37 +0530

    added background gradient to hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -4,27 +4,35 @@ import { HeroScrollToContent } from "../svgs/HeroScrollToContent";
 
 export const Hero = component$(() => {
   return (
-    <div className="flex flex-col items-center justify-between pt-12 p-5 relative h-[calc(100vh-80px)]">
-      <HeroBackground className="fixed top-1/3 -z-[1]" />
-      <h1 class="text-5xl md:text-7xl lg:text-8xl leading-snug font-medium">
-        Meet the Humans of{" "}
-        <span
-          class="bg-gradient-to-r bg-clip-text  text-transparent 
+    <>
+      <div className="flex flex-col items-center justify-between pt-12 p-5 relative h-[calc(100vh-80px)]">
+        <HeroBackground className="fixed top-1/3 -z-[1]" />
+        <h1 class="text-5xl md:text-7xl lg:text-8xl leading-snug font-medium">
+          Meet the Humans of{" "}
+          <span
+            class="bg-gradient-to-r bg-clip-text  text-transparent 
             from-white via-pink-500 to-yellow-500
             animate-text"
-        >
-          BrainDAO{" "}
-        </span>
-        and builders of IQ.Wiki.
-      </h1>
-      <p class="p-4 md:p-8 rounded-md md:rounded-none max-w-md bg-white/25 backdrop-blur-md  ml-auto">
-        IQ.Wiki's mission is to bring the world's knowledge on-chain through the
-        IQ token.
-      </p>
-      <div class="flex flex-col justify-center items-center gap-3 text-pink-400 text-lg">
-        <p>Get to know us</p>
-        <HeroScrollToContent />
+          >
+            BrainDAO{" "}
+          </span>
+          and builders of IQ.Wiki.
+        </h1>
+        <p class="ml-auto p-4 md:p-8 rounded-md md:rounded-none max-w-md bg-white/10 backdrop-blur-md">
+          IQ.Wiki's mission is to bring the world's knowledge on-chain through
+          the IQ token.
+        </p>
+        <div class="flex flex-col justify-center items-center gap-3 text-pink-400 text-lg">
+          <p>Get to know us</p>
+          <HeroScrollToContent />
+        </div>
       </div>
-    </div>
+
+      <div class="absolute w-screen h-screen top-0 left-0">
+        <div class="relative w-full h-full overflow-hidden">
+          <div class="absolute rounded-full h-[60vw] w-[60vw] top-1/4 left-1/2 bg-gradient-radial from-pink-400/30 via-transparent to-transparent" />
+        </div>
+      </div>
+    </>
   );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,26 @@ module.exports = {
           },
         },
       },
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(ellipse at center, var(--tw-gradient-stops))",
+        "gradient-radial-at-t":
+          "radial-gradient(ellipse at top, var(--tw-gradient-stops))",
+        "gradient-radial-at-b":
+          "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
+        "gradient-radial-at-l":
+          "radial-gradient(ellipse at left, var(--tw-gradient-stops))",
+        "gradient-radial-at-r":
+          "radial-gradient(ellipse at right, var(--tw-gradient-stops))",
+        "gradient-radial-at-tl":
+          "radial-gradient(ellipse at top left, var(--tw-gradient-stops))",
+        "gradient-radial-at-tr":
+          "radial-gradient(ellipse at top right, var(--tw-gradient-stops))",
+        "gradient-radial-at-bl":
+          "radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))",
+        "gradient-radial-at-br":
+          "radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [
