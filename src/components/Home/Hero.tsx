c135a3c9ed65f072d31a013eeec32356cbba627c
Srujan Gurram <srujangs8@gmail.com>
2022-10-14 23:44:53 +0530

    fixed elements not clickable and made structure more responsive

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -5,7 +5,7 @@ import { HeroScrollToContent } from "../svgs/HeroScrollToContent";
 export const Hero = component$(() => {
   return (
     <>
-      <div class="flex flex-col items-center justify-between pt-12 p-5 h-[1200px]">
+      <div class="flex flex-col items-center justify-between pt-12 p-5">
         <h1 class="text-5xl md:text-7xl lg:text-[115px] leading-snug font-medium">
           Meet the Humans of{" "}
           <span
@@ -17,23 +17,19 @@ export const Hero = component$(() => {
           </span>
           and Builders of IQ.Wiki.
         </h1>
+        <HeroBackground className="-mt-64 -z-10 w-screen h-[100vw] max-h-[56rem] max-w-4xl" />
         <div class="flex flex-col gap-[10vh] w-full">
-          <p class="ml-auto p-4 md:p-8 text-lg text-center rounded-md md:rounded-none max-w-md bg-white/10 backdrop-blur-md">
-            IQ.Wiki's mission is to bring the world's knowledge on-chain through
-            the IQ token.
-          </p>
+          <div class="relative -mt-10 md:-mt-32">
+            <div class="absolute rounded-full right-0 -top-36 h-[400px] w-[400px] bg-gradient-radial-at-tl from-[#4b7acb]/20 via-[#cc4c7f]/20 to-transparent blur-[120px]" />
+            <p class="ml-auto p-4 md:p-8 text-md lg:text-lg text-center rounded-md md:rounded-none max-w-md bg-white/10 backdrop-blur-md">
+              IQ.Wiki's mission is to bring the world's knowledge on-chain
+              through the IQ token.
+            </p>
+          </div>
           <div class="flex flex-col justify-center items-center gap-3 text-pink-400 text-lg">
             <p>Get to know us</p>
             <HeroScrollToContent />
           </div>
-        </div>
-      </div>
-
-      {/* POSITION FIXED ELEMENTS */}
-      <div class="absolute w-screen h-[170vh] top-0 left-0">
-        <HeroBackground className="absolute left-[50%] top-[220px] -translate-x-1/2  -z-[1] max-w-screen" />
-        <div class="relative w-full h-full overflow-hidden">
-          <div class="absolute rounded-full h-[400px] w-[400px] left-1/2 md:translate-x-[30%] top-[800px] bg-gradient-radial-at-tl from-[#4b7acb]/30 to-[#cc4c7f]/30 blur-[120px]" />
         </div>
       </div>
     </>
