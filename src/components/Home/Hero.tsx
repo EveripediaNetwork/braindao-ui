8c3667d47a75020d9f70a747af0e724eb3b05f2c
Srujan Gurram <srujangs8@gmail.com>
2022-10-14 21:44:19 +0530

    fixed design implimentation to sync design

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -5,7 +5,7 @@ import { HeroScrollToContent } from "../svgs/HeroScrollToContent";
 export const Hero = component$(() => {
   return (
     <>
-      <div class="flex flex-col items-center justify-between pt-12 p-5 h-[calc(150vh)]">
+      <div class="flex flex-col items-center justify-between pt-12 p-5 h-[1200px]">
         <h1 class="text-5xl md:text-7xl lg:text-[115px] leading-snug font-medium">
           Meet the Humans of{" "}
           <span
@@ -31,9 +31,9 @@ export const Hero = component$(() => {
 
       {/* POSITION FIXED ELEMENTS */}
       <div class="absolute w-screen h-[170vh] top-0 left-0">
-        <HeroBackground className="absolute left-[20%] top-[15%] -z-[1]" />
+        <HeroBackground className="absolute left-[50%] top-[220px] -translate-x-1/2  -z-[1] max-w-screen" />
         <div class="relative w-full h-full overflow-hidden">
-          <div class="absolute rounded-full h-[1000px] w-[1000px] top-[70vh] left-[40%] md:top-[70vh] -translate-x-1/2 -translate-y-1/2 md:translate-x-0 md:translate-y-0 bg-gradient-radial from-pink-400/30 via-transparent to-transparent" />
+          <div class="absolute rounded-full h-[400px] w-[400px] left-1/2 md:translate-x-[30%] top-[800px] bg-gradient-radial-at-tl from-[#4b7acb]/30 to-[#cc4c7f]/30 blur-[120px]" />
         </div>
       </div>
     </>
--- src/components/Home/OurStory.tsx
+++ src/components/Home/OurStory.tsx
@@ -1,7 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 
 export const OurStory = component$(() => (
-  <div class="flex flex-col h-screen justify-center items-center my-10">
-    <h2 class="text-5xl font-bold">Our Story</h2>
-  </div>
+  <div class="flex flex-col h-screen justify-center items-center my-10"></div>
 ));
