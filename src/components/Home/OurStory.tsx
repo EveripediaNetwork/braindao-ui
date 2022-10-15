6ffa89b7fd02818249d6a1dbd39dff928322e8ce
Srujan Gurram <srujangs8@gmail.com>
2022-10-15 21:11:11 +0530

    added our story gradient background

--- src/components/Home/OurStory.tsx
+++ src/components/Home/OurStory.tsx
@@ -5,7 +5,7 @@ import { BgGradient } from "../shared/BgGradient";
 export const OurStory = component$(() => {
   const storyStore = useStore(ourStory[1]);
   return (
-    <div class="relative overflow-hidden md:overflow-visible  flex flex-col justify-center items-center my-20">
+    <div class="relative overflow-hidden md:overflow-visible  flex flex-col justify-center items-center py-20">
       <h2 class="text-5xl font-bold">Our Story</h2>
       <nav class="flex items-center gap-4 md:gap-10 mt-10 children:text-xl">
         {ourStory.map((story) => (
@@ -22,7 +22,7 @@ export const OurStory = component$(() => {
           </button>
         ))}
       </nav>
-      <BgGradient className="left-0 top-7" />
+      <BgGradient className="left-0 top-20" />
       <div class="mt-10 mx-5 max-w-4xl text-center bg-white/10 p-5 md:p-16 border-[1px] border-white/10 backdrop-blur-md">
         {storyStore.content}
       </div>
--- src/components/shared/BgGradient.tsx
+++ src/components/shared/BgGradient.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export const BgGradient = component$(
   ({ className }: { className?: string }) => (
     <div
-      class={`absolute pointer-events-none rounded-full right-0 -top-36 h-[400px] w-[400px] bg-gradient-radial-at-tl from-[#4b7acb]/20 via-[#cc4c7f]/20 to-transparent blur-[120px] ${className}`}
+      class={`absolute pointer-events-none rounded-full right-0 -top-36 h-[350px] w-[350px] bg-gradient-radial-at-tl from-[#4b7acb]/20 to-[#cc4c7f]/20 blur-[120px] ${className}`}
     />
   )
 );
