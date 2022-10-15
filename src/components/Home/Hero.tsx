6b430d8a1b47de3c04be1880c2beb7c208c9168a
Srujan Gurram <srujangs8@gmail.com>
2022-10-15 21:02:54 +0530

    added our story section to homepage

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { HeroBackground } from "~/components/svgs/HeroBackground";
+import { BgGradient } from "../shared/BgGradient";
 import { HeroScrollToContent } from "../svgs/HeroScrollToContent";
 
 export const Hero = component$(() => {
@@ -20,7 +21,7 @@ export const Hero = component$(() => {
         <HeroBackground className="-mt-64 -z-10 w-screen h-[100vw] max-h-[56rem] max-w-4xl" />
         <div class="flex flex-col gap-[10vh] w-full">
           <div class="relative -mt-10 md:-mt-32">
-            <div class="absolute rounded-full right-0 -top-36 h-[400px] w-[400px] bg-gradient-radial-at-tl from-[#4b7acb]/20 via-[#cc4c7f]/20 to-transparent blur-[120px]" />
+            <BgGradient />
             <p class="ml-auto p-4 md:p-8 text-md lg:text-lg text-center rounded-md md:rounded-none max-w-md bg-white/10 backdrop-blur-md">
               IQ.Wiki's mission is to bring the world's knowledge on-chain
               through the IQ token.
--- src/components/Home/OurStory.tsx
+++ src/components/Home/OurStory.tsx
@@ -1,5 +1,31 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
+import { ourStory } from "~/data/our-story";
+import { BgGradient } from "../shared/BgGradient";
 
-export const OurStory = component$(() => (
-  <div class="flex flex-col h-screen justify-center items-center my-10"></div>
-));
+export const OurStory = component$(() => {
+  const storyStore = useStore(ourStory[1]);
+  return (
+    <div class="relative overflow-hidden md:overflow-visible  flex flex-col justify-center items-center my-20">
+      <h2 class="text-5xl font-bold">Our Story</h2>
+      <nav class="flex items-center gap-4 md:gap-10 mt-10 children:text-xl">
+        {ourStory.map((story) => (
+          <button
+            class={
+              storyStore.year === story.year ? "!text-3xl text-pink-500" : ""
+            }
+            onClick$={() => {
+              storyStore.content = story.content;
+              storyStore.year = story.year;
+            }}
+          >
+            {story.year}
+          </button>
+        ))}
+      </nav>
+      <BgGradient className="left-0 top-7" />
+      <div class="mt-10 mx-5 max-w-4xl text-center bg-white/10 p-5 md:p-16 border-[1px] border-white/10 backdrop-blur-md">
+        {storyStore.content}
+      </div>
+    </div>
+  );
+});
