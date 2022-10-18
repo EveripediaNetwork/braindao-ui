e1823b95daf944b885437f5b5483d2c0140c8b62
Srujan Gurram <srujangs8@gmail.com>
2022-10-18 19:34:14 +0530

    added plus icon to member images

--- src/components/Home/TheTeam/TeamMemberCard.tsx
+++ src/components/Home/TheTeam/TeamMemberCard.tsx
@@ -1,9 +1,13 @@
 import { component$ } from "@builder.io/qwik";
+import { PlusIcon } from "~/components/icons/Plus";
 import { TeamMember } from "~/data/the-team";
 
 export const TeamMemberCard = component$(
   ({ member }: { member: TeamMember }) => (
-    <div class="flex flex-col items-center text-center">
+    <div class="relative flex flex-col items-center text-center">
+      <button class="absolute grid place-items-center bg-black h-8 w-8 right-0">
+        <PlusIcon className="fill-white" />
+      </button>
       <img class="object-cover -z-10 w-[250px] h-[300px]" src={member.image} />
       <h3 class="uppercase z-20 font-bebas_neue -mt-4 text-4xl">
         {member.name}
