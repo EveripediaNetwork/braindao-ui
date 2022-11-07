3f32c83ecc86e56178d4bde5f4d4298551dddbe5
Adebesin Cell <adebesintolulope80@gmail.com>
2022-11-07 10:07:00 +0100

    Fixes external links open new tabs

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,57 +1,63 @@
-import { component$, useSignal } from "@builder.io/qwik";
-import { HamburgerIcon } from "../icons/HamburgerIcon";
-import { RightArrowIcon } from "../icons/RightArrow";
-import { BraindaoLogo } from "../svgs/BraindaoLogo";
+import { component$, useSignal } from '@builder.io/qwik';
+import { HamburgerIcon } from '../icons/HamburgerIcon';
+import { RightArrowIcon } from '../icons/RightArrow';
+import { BraindaoLogo } from '../svgs/BraindaoLogo';
 
 export const Navbar = component$(() => {
   const mobileNav = useSignal<HTMLDivElement>();
   return (
-    <header class="flex flex-col backdrop-blur-xl bg-[#121212]/60 z-50 lg:-mx-7 lg:px-14 p-3">
-      <div class="flex justify-between items-center w-full">
-        <h1 class="flex gap-2 items-center text-lg font-medium">
+    <header class='flex flex-col backdrop-blur-xl bg-[#121212]/60 z-50 lg:-mx-7 lg:px-14 p-3'>
+      <div class='flex justify-between items-center w-full'>
+        <h1 class='flex gap-2 items-center text-lg font-medium'>
           <BraindaoLogo /> BrainDAO
         </h1>
-        <nav class="hidden md:flex gap-20 text-sm">
-          <a href="https://iq.wiki">IQ WIKI</a>
-          <a href="https://learn.everipedia.org/iq/">Learn</a>
-          <a href="https://iq.wiki/blog">Blog</a>
+        <nav class='hidden md:flex gap-20 text-sm'>
+          <a target='_blank' href='https://iq.wiki'>
+            IQ WIKI
+          </a>
+          <a target='_blank' href='https://learn.everipedia.org/iq/'>
+            Learn
+          </a>
+          <a target='_blank' href='https://iq.wiki/blog'>
+            Blog
+          </a>
         </nav>
         <a
-          href="https://iq.braindao.org"
-          target="_blank"
-          class="hidden md:block bg-pink-500 text-sm px-5 py-2 rounded-md  hover:bg-white hover:text-black transition-all ease-in-out duration-300"
+          href='https://iq.braindao.org'
+          target='_blank'
+          class='hidden md:block bg-pink-500 text-sm px-5 py-2 rounded-md  hover:bg-white hover:text-black transition-all ease-in-out duration-300'
         >
           Launch IQ Dashboard
         </a>
         <button
-          class="fill-white md:hidden"
+          class='fill-white md:hidden'
           onClick$={() => {
-            mobileNav.value?.classList.toggle("hidden");
-            mobileNav.value?.classList.toggle("block");
+            mobileNav.value?.classList.toggle('hidden');
+            mobileNav.value?.classList.toggle('block');
           }}
         >
           <HamburgerIcon />
         </button>
       </div>
-      <div ref={mobileNav} class="hidden md:hidden">
-        <ul class="flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3">
+      <div ref={mobileNav} class='hidden md:hidden'>
+        <ul class='flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3'>
           <li>
-            <a href="https://iq.wiki">IQ WIKI</a>
+            <a href='https://iq.wiki'>IQ WIKI</a>
           </li>
           <li>
-            <a href="https://learn.everipedia.org/iq/">Learn</a>
+            <a href='https://learn.everipedia.org/iq/'>Learn</a>
           </li>
           <li>
-            <a href="https://iq.wiki/blog">Blog</a>
+            <a href='https://iq.wiki/blog'>Blog</a>
           </li>
           <li>
             <a
-              href="https://iq.braindao.org"
-              target="_blank"
-              class="flex justify-between"
+              href='https://iq.braindao.org'
+              target='_blank'
+              class='flex justify-between'
             >
               <span>IQ Dashboard</span>
-              <RightArrowIcon className="fill-white" />
+              <RightArrowIcon className='fill-white' />
             </a>
           </li>
         </ul>
