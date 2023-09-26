810375f7d3f971df7f3c05d24bb4e13b1ffd3fb7
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-09-26 19:49:08 +0100

    biome formatting

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,80 +1,80 @@
-import React from 'react';
-import { BraindaoLogoDark } from '../svgs/BraindaoLogoDark';
-import { RightArrowIcon } from '../icons/RightArrow';
-import NavBarButton from './NavBarButton';
-import { navLinks } from '@/data/Nav';
-import { raleway } from '@/app/font';
-import dynamic from 'next/dynamic';
-import { CgSpinner } from 'react-icons/cg';
+import React from "react";
+import { BraindaoLogoDark } from "../svgs/BraindaoLogoDark";
+import { RightArrowIcon } from "../icons/RightArrow";
+import NavBarButton from "./NavBarButton";
+import { navLinks } from "@/data/Nav";
+import { raleway } from "@/app/font";
+import dynamic from "next/dynamic";
+import { CgSpinner } from "react-icons/cg";
 
 const ThemeSwitcher = dynamic(
-	() => import('@/components/layouts/theme/ThemeSwitcher'),
-	{
-		ssr: false,
-		loading: () => <CgSpinner size={36} className='animate-spin' />,
-	}
+  () => import("@/components/layouts/theme/ThemeSwitcher"),
+  {
+    ssr: false,
+    loading: () => <CgSpinner size={36} className="animate-spin" />,
+  }
 );
 
 const Navbar = () => {
-	return (
-		<div className='bg-black text-whiteAlpha-900'>
-			<div className='max-w-[1536px] mx-auto'>
-				<header className=' flex flex-col z-50 lg:-mx-7 lg:px-14 p-3'>
-					<div className='flex justify-between items-center w-full'>
-						<h1 className='flex gap-2 items-center text-lg font-medium'>
-							<a href='/'>
-								<BraindaoLogoDark />
-							</a>
-						</h1>
-						<nav className='hidden md:flex gap-20 text-sm lg:text-base'>
-							{navLinks.map((link) => (
-								<a
-									target='_blank'
-									href={link.href}
-									key={link.href}
-									className=''
-								>
-									{link.title}
-								</a>
-							))}
-						</nav>
-						<div className='flex gap-2 items-center'>
-							<a
-								href='https://iq.braindao.org'
-								target='_blank'
-								className={`hidden md:block bg-pink-500 text-sm lg:text-base px-5 py-2 rounded-md  hover:bg-white hover:text-black transition-all ease-in-out duration-300 ${raleway.className}`}
-							>
-								Launch App
-							</a>
-							<ThemeSwitcher />
-						</div>
-						<NavBarButton />
-					</div>
-					<div id='mobile_nav_items' className='hidden md:hidden'>
-						<ul className='flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3'>
-							{navLinks.map((link) => (
-								<li key={link.href}>
-									<a target='_blank' href={link.href}>
-										{link.title}
-									</a>
-								</li>
-							))}
-							<li>
-								<a
-									href='https://iq.braindao.org'
-									target='_blank'
-									className='flex justify-between'
-								>
-									<span>IQ Dashboard</span>
-									<RightArrowIcon className='fill-white' />
-								</a>
-							</li>
-						</ul>
-					</div>
-				</header>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-black text-whiteAlpha-900">
+      <div className="max-w-[1536px] mx-auto">
+        <header className=" flex flex-col z-50 lg:-mx-7 lg:px-14 p-3">
+          <div className="flex justify-between items-center w-full">
+            <h1 className="flex gap-2 items-center text-lg font-medium">
+              <a href="/">
+                <BraindaoLogoDark />
+              </a>
+            </h1>
+            <nav className="hidden md:flex gap-20 text-sm lg:text-base">
+              {navLinks.map((link) => (
+                <a
+                  target="_blank"
+                  href={link.href}
+                  key={link.href}
+                  className=""
+                >
+                  {link.title}
+                </a>
+              ))}
+            </nav>
+            <div className="flex gap-2 items-center">
+              <a
+                href="https://iq.braindao.org"
+                target="_blank"
+                className={`hidden md:block bg-pink-500 text-sm lg:text-base px-5 py-2 rounded-md  hover:bg-white hover:text-black transition-all ease-in-out duration-300 ${raleway.className}`}
+              >
+                Launch App
+              </a>
+              <ThemeSwitcher />
+            </div>
+            <NavBarButton />
+          </div>
+          <div id="mobile_nav_items" className="hidden md:hidden">
+            <ul className="flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3">
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <a target="_blank" href={link.href}>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+              <li>
+                <a
+                  href="https://iq.braindao.org"
+                  target="_blank"
+                  className="flex justify-between"
+                >
+                  <span>IQ Dashboard</span>
+                  <RightArrowIcon className="fill-white" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/layouts/ecosystem/IqDashboard.tsx
+++ src/components/layouts/ecosystem/IqDashboard.tsx
@@ -1,53 +1,53 @@
-'use client';
+"use client";
 
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { StatsPointers, TokenBrief } from '../Iqtoken';
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { StatsPointers, TokenBrief } from "../Iqtoken";
 
 const IqDashboard = () => {
-	const { theme } = useTheme();
-	return (
-		<div className='max-w-[1536px] mx-auto flex flex-col gap-16 py-24'>
-			<div className='px-[120px]'>
-				<TokenBrief
-					title='IQ Dashboard'
-					description='Utilizing the IQ Dashboard, you have the ability to stake your tokens, facilitate cross-blockchain bridging, participate in governance proposal voting, and engage in numerous other activities within the blockchain ecosystem.'
-					action='https://iq.braindao.org/dashboard'
-					buttonText='Explore IQ Dashboard'
-				/>
-				<div className='flex justify-between py-3 mt-16 w-full'>
-					<StatsPointers
-						title='Swap'
-						headerSize='text-xl'
-						content='Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms.'
-						className=' justify-between'
-					/>
-					<StatsPointers
-						title='Bridge'
-						headerSize='text-xl'
-						content='Transfer IQ from EOS to ETH and vice versa using the IQ bridge
-'
-						className='justify-between'
-					/>
-					<StatsPointers
-						title='Bridge'
-						headerSize='text-xl'
-						content='Lock IQ token over a period of time and earn IQ token rewards.
-'
-						className='justify-between'
-					/>
-					<StatsPointers
-						title='Voting'
-						headerSize='text-xl'
-						content='Stakers can vote on all governance proposals and create their own proposals.'
-						className='justify-between'
-					/>
-				</div>
-			</div>
-			<div className='w-full h-[887px] bg-iq-dashboard-mockup dark:bg-iq-dashboard-mockup-dark bg-no-repeat bg-center bg-cover'></div>
-		</div>
-	);
+  const { theme } = useTheme();
+  return (
+    <div className="max-w-[1536px] mx-auto flex flex-col gap-16 py-24">
+      <div className="px-[120px]">
+        <TokenBrief
+          title="IQ Dashboard"
+          description="Utilizing the IQ Dashboard, you have the ability to stake your tokens, facilitate cross-blockchain bridging, participate in governance proposal voting, and engage in numerous other activities within the blockchain ecosystem."
+          action="https://iq.braindao.org/dashboard"
+          buttonText="Explore IQ Dashboard"
+        />
+        <div className="flex justify-between py-3 mt-16 w-full">
+          <StatsPointers
+            title="Swap"
+            headerSize="text-xl"
+            content="Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms."
+            className=" justify-between"
+          />
+          <StatsPointers
+            title="Bridge"
+            headerSize="text-xl"
+            content="Transfer IQ from EOS to ETH and vice versa using the IQ bridge
+"
+            className="justify-between"
+          />
+          <StatsPointers
+            title="Bridge"
+            headerSize="text-xl"
+            content="Lock IQ token over a period of time and earn IQ token rewards.
+"
+            className="justify-between"
+          />
+          <StatsPointers
+            title="Voting"
+            headerSize="text-xl"
+            content="Stakers can vote on all governance proposals and create their own proposals."
+            className="justify-between"
+          />
+        </div>
+      </div>
+      <div className="w-full h-[887px] bg-iq-dashboard-mockup dark:bg-iq-dashboard-mockup-dark bg-no-repeat bg-center bg-cover"></div>
+    </div>
+  );
 };
 
 export default IqDashboard;
