c11211c3fdb38ca75650bb2f66f7d9529e4e5da2
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-09-28 17:01:58 +0100

    working on team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Ecosystem from "@/components/layouts/ecosystem";
-import Hero from "@/components/layouts/Hero";
-import Iqtoken from "@/components/layouts/Iqtoken";
+import BraindaoTeam from '@/components/layouts/braindao-team';
+import Ecosystem from '@/components/layouts/ecosystem';
+import Hero from '@/components/layouts/Hero';
+import Iqtoken from '@/components/layouts/Iqtoken';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Iqtoken />
-      <Ecosystem />
-    </>
-  );
+	return (
+		<>
+			<Hero />
+			<Iqtoken />
+			<Ecosystem />
+			<BraindaoTeam />
+		</>
+	);
 }
