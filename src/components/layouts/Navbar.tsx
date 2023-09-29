c571e5ad385dba79b4588b744c087d5077fd83bb
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-09-29 10:34:15 +0100

    biome formatting

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,80 +1,80 @@
-import React from 'react';
-import { BraindaoLogoDark } from '../svgs/BraindaoLogoDark';
-import { RightArrowIcon } from '../icons/RightArrow';
-import NavBarButton from './NavBarButton';
-import { navLinks } from '@/data/Nav';
-import { raleway } from '@/app/font';
-import dynamic from 'next/dynamic';
-import { CgSpinner } from 'react-icons/cg';
+import React from "react";
+import { BraindaoLogoDark } from "../svgs/BraindaoLogoDark";
+import { RightArrowIcon } from "../icons/RightArrow";
+import NavBarButton from "./NavBarButton";
+import { navLinks } from "@/data/Nav";
+import { raleway } from "@/app/font";
+import dynamic from "next/dynamic";
+import { CgSpinner } from "react-icons/cg";
 
 const ThemeSwitcher = dynamic(
-	() => import('@/components/layouts/theme/ThemeSwitcher'),
-	{
-		ssr: false,
-		loading: () => <CgSpinner size={36} className='animate-spin' />,
-	}
+  () => import("@/components/layouts/theme/ThemeSwitcher"),
+  {
+    ssr: false,
+    loading: () => <CgSpinner size={36} className="animate-spin" />,
+  }
 );
 
 const Navbar = () => {
-	return (
-		<div className='bg-transparent text-whiteAlpha-900'>
-			<div className='max-w-[1536px] mx-auto'>
-				<header className=' flex flex-col z-50 lg:-mx-7 lg:px-14 p-3'>
-					<div className='flex justify-between items-center w-full'>
-						<h1 className='flex gap-2 items-center text-lg font-medium'>
-							<a href='/'>
-								<BraindaoLogoDark />
-							</a>
-						</h1>
-						<nav className='hidden lg:flex gap-10 xl:gap-20 text-sm lg:text-base'>
-							{navLinks.map((link) => (
-								<a
-									target={link.target}
-									href={link.href}
-									key={link.href}
-									className=''
-								>
-									{link.title}
-								</a>
-							))}
-						</nav>
-						<div className='flex gap-2 items-center'>
-							<a
-								href='https://iq.braindao.org'
-								target='_blank'
-								className={`hidden md:flex flex-1 justify-center bg-brand-500 dark:bg-brand-700 hover:bg-brand-700 md:w-[184px] text-sm lg:text-base px-5 py-2 rounded-md transition-all ease-in-out duration-300 ${raleway.className}`}
-							>
-								Launch App
-							</a>
-							<ThemeSwitcher />
-							<NavBarButton />
-						</div>
-					</div>
-					<div id='mobile_nav_items' className='hidden z-10'>
-						<ul className='flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3'>
-							{navLinks.map((link) => (
-								<li key={link.href}>
-									<a target={link.target} href={link.href}>
-										{link.title}
-									</a>
-								</li>
-							))}
-							<li>
-								<a
-									href='https://iq.braindao.org'
-									target='_blank'
-									className='flex justify-between'
-								>
-									<span>IQ Dashboard</span>
-									<RightArrowIcon className='fill-white' />
-								</a>
-							</li>
-						</ul>
-					</div>
-				</header>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-transparent text-whiteAlpha-900">
+      <div className="max-w-[1536px] mx-auto">
+        <header className=" flex flex-col z-50 lg:-mx-7 lg:px-14 p-3">
+          <div className="flex justify-between items-center w-full">
+            <h1 className="flex gap-2 items-center text-lg font-medium">
+              <a href="/">
+                <BraindaoLogoDark />
+              </a>
+            </h1>
+            <nav className="hidden lg:flex gap-10 xl:gap-20 text-sm lg:text-base">
+              {navLinks.map((link) => (
+                <a
+                  target={link.target}
+                  href={link.href}
+                  key={link.href}
+                  className=""
+                >
+                  {link.title}
+                </a>
+              ))}
+            </nav>
+            <div className="flex gap-2 items-center">
+              <a
+                href="https://iq.braindao.org"
+                target="_blank"
+                className={`hidden md:flex flex-1 justify-center bg-brand-500 dark:bg-brand-700 hover:bg-brand-700 md:w-[184px] text-sm lg:text-base px-5 py-2 rounded-md transition-all ease-in-out duration-300 ${raleway.className}`}
+              >
+                Launch App
+              </a>
+              <ThemeSwitcher />
+              <NavBarButton />
+            </div>
+          </div>
+          <div id="mobile_nav_items" className="hidden z-10">
+            <ul className="flex flex-col divide-y-[1px] max-w-xl mx-auto divide-white/25 p-5 text-lg children:p-3">
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <a target={link.target} href={link.href}>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+              <li>
+                <a
+                  href="https://iq.braindao.org"
+                  target="_blank"
+                  className="flex justify-between"
+                >
+                  <span>IQ Dashboard</span>
+                  <RightArrowIcon className="fill-white" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/data/Nav.ts
+++ src/data/Nav.ts
@@ -1,27 +1,27 @@
 export const navLinks = [
-	{
-		title: 'IQ.wiki',
-		href: '#iq-wiki',
-		target: '',
-	},
-	{
-		title: 'IQ GPT',
-		href: '#iq-gpt',
-		target: '',
-	},
-	{
-		title: 'IQ Treasury',
-		href: 'https://iq.braindao.org/dashboard/treasury',
-		target: '_blank',
-	},
-	{
-		title: 'Learn',
-		href: 'https://learn.everipedia.org/iq',
-		target: '_blank',
-	},
-	{
-		title: 'Blog',
-		href: 'https://iq.wiki/blog',
-		target: '_blank',
-	},
+  {
+    title: "IQ.wiki",
+    href: "#iq-wiki",
+    target: "",
+  },
+  {
+    title: "IQ GPT",
+    href: "#iq-gpt",
+    target: "",
+  },
+  {
+    title: "IQ Treasury",
+    href: "https://iq.braindao.org/dashboard/treasury",
+    target: "_blank",
+  },
+  {
+    title: "Learn",
+    href: "https://learn.everipedia.org/iq",
+    target: "_blank",
+  },
+  {
+    title: "Blog",
+    href: "https://iq.wiki/blog",
+    target: "_blank",
+  },
 ];
