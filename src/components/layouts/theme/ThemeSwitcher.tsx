bd0c458f2fef81279bfad300d77429002cd7d720
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-09-29 17:12:42 +0100

    done with iterations

--- src/components/layouts/theme/ThemeSwitcher.tsx
+++ src/components/layouts/theme/ThemeSwitcher.tsx
@@ -1,31 +1,31 @@
-"use client";
+'use client';
 
-import { useTheme } from "next-themes";
-import { RiMoonFill, RiSunFill } from "react-icons/ri";
+import { useTheme } from 'next-themes';
+import { RiMoonFill, RiSunFill } from 'react-icons/ri';
 
 const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme();
+	const { theme, setTheme } = useTheme();
 
-  const isDarkTheme = theme === "dark";
+	const isDarkTheme = theme === 'dark';
 
-  const handleThemeToggle = () => {
-    if (isDarkTheme) {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-  };
+	const handleThemeToggle = () => {
+		if (isDarkTheme) {
+			setTheme('light');
+		} else {
+			setTheme('dark');
+		}
+	};
 
-  return (
-    <button
-      type="button"
-      className="hidden xl:flex cursor-pointer w-10 h-full  justify-center px-1 py-2 items-center rounded-md border border-whiteAlpha-400 text-whiteAlpha900"
-      onClick={handleThemeToggle}
-      aria-label="Theme Switcher"
-    >
-      {isDarkTheme ? <RiSunFill size={24} /> : <RiMoonFill size={24} />}
-    </button>
-  );
+	return (
+		<button
+			type='button'
+			className='hidden lg:flex cursor-pointer w-10 h-full  justify-center px-1 py-2 items-center rounded-md border border-whiteAlpha-400 text-whiteAlpha900'
+			onClick={handleThemeToggle}
+			aria-label='Theme Switcher'
+		>
+			{isDarkTheme ? <RiSunFill size={24} /> : <RiMoonFill size={24} />}
+		</button>
+	);
 };
 
 export default ThemeSwitcher;
