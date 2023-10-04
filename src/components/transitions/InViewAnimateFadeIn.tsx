99808e724fa26db66b4861b73d95889a140e3b36
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-04 13:19:27 +0100

    biome formatting

--- src/components/transitions/InViewAnimateFadeIn.tsx
+++ src/components/transitions/InViewAnimateFadeIn.tsx
@@ -1,44 +1,44 @@
-'use client';
+"use client";
 
-import { useAnimation, motion } from 'framer-motion';
-import React, { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useAnimation, motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export type TInViewAnimate = {
-	children: React.ReactNode;
-	className?: string;
-	delay?: number;
+  children: React.ReactNode;
+  className?: string;
+  delay?: number;
 };
 const InViewAnimateFadeIn: React.FC<TInViewAnimate> = ({
-	children,
-	className,
-	delay,
+  children,
+  className,
+  delay,
 }) => {
-	const { ref, inView } = useInView();
-	const animation = useAnimation();
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
 
-	useEffect(() => {
-		if (inView) {
-			animation.start({
-				opacity: 1,
-				transition: {
-					duration: 0.8,
-					delay: delay ? delay : 0.2,
-				},
-			});
-		}
-		if (!inView) {
-			animation.start({
-				opacity: 0,
-			});
-		}
-	}, [animation, delay, inView]);
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        transition: {
+          duration: 0.8,
+          delay: delay ? delay : 0.2,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({
+        opacity: 0,
+      });
+    }
+  }, [animation, delay, inView]);
 
-	return (
-		<motion.div ref={ref} animate={animation} className={`${className}`}>
-			{children}
-		</motion.div>
-	);
+  return (
+    <motion.div ref={ref} animate={animation} className={`${className}`}>
+      {children}
+    </motion.div>
+  );
 };
 
 export default InViewAnimateFadeIn;
