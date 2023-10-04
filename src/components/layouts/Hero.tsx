48bf47be291711042f6c7e9e0043ef0859d9c12e
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-04 07:47:35 +0100

    biome formatting

--- src/components/layouts/Hero.tsx
+++ src/components/layouts/Hero.tsx
@@ -1,81 +1,81 @@
-'use client';
+"use client";
 
-import { space_grotesk } from '@/app/font';
-import gsap from 'gsap';
-import React, { MouseEventHandler, useRef, useState } from 'react';
-import { RiArrowDownDoubleLine } from 'react-icons/ri';
-import Navbar from './Navbar';
+import { space_grotesk } from "@/app/font";
+import gsap from "gsap";
+import React, { MouseEventHandler, useRef, useState } from "react";
+import { RiArrowDownDoubleLine } from "react-icons/ri";
+import Navbar from "./Navbar";
 
 const Hero = () => {
-	const plane = useRef<HTMLDivElement>(null);
+  const plane = useRef<HTMLDivElement>(null);
 
-	let requestAnimationFrameId: number | null = null;
-	let xForce = 0;
-	let yForce = 0;
-	const easing = 0.08;
-	const speed = 0.01;
+  let requestAnimationFrameId: number | null = null;
+  let xForce = 0;
+  let yForce = 0;
+  const easing = 0.08;
+  const speed = 0.01;
 
-	const manageMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
-		const { movementX, movementY } = e;
-		xForce += movementX * speed;
-		yForce += movementY * speed;
+  const manageMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    const { movementX, movementY } = e;
+    xForce += movementX * speed;
+    yForce += movementY * speed;
 
-		if (requestAnimationFrameId == null) {
-			requestAnimationFrameId = requestAnimationFrame(animate);
-		}
-	};
+    if (requestAnimationFrameId == null) {
+      requestAnimationFrameId = requestAnimationFrame(animate);
+    }
+  };
 
-	const lerp = (start: number, target: number, amount: number) =>
-		start * (1 - amount) + target * amount;
+  const lerp = (start: number, target: number, amount: number) =>
+    start * (1 - amount) + target * amount;
 
-	const animate = () => {
-		xForce = lerp(xForce, 0, easing);
-		yForce = lerp(yForce, 0, easing);
-		gsap.set(plane.current, { x: `+=${xForce}`, y: `+=${yForce}` });
+  const animate = () => {
+    xForce = lerp(xForce, 0, easing);
+    yForce = lerp(yForce, 0, easing);
+    gsap.set(plane.current, { x: `+=${xForce}`, y: `+=${yForce}` });
 
-		if (Math.abs(xForce) < 0.01) xForce = 0;
-		if (Math.abs(yForce) < 0.01) yForce = 0;
+    if (Math.abs(xForce) < 0.01) xForce = 0;
+    if (Math.abs(yForce) < 0.01) yForce = 0;
 
-		if (xForce != 0 || yForce != 0) {
-			requestAnimationFrame(animate);
-		} else {
-			cancelAnimationFrame(requestAnimationFrameId!);
-			requestAnimationFrameId = null;
-		}
-	};
+    if (xForce != 0 || yForce != 0) {
+      requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(requestAnimationFrameId!);
+      requestAnimationFrameId = null;
+    }
+  };
 
-	return (
-		<div onMouseMove={manageMouseMove}>
-			<div className='relative bg-braindao-sm sm:bg-hero bg-cover bg-no-repeat bg-center text-whiteAlpha-900'>
-				<Navbar />
-				<div
-					ref={plane}
-					className='hidden sm:block bg-brainie bg-cover bg-center bg-no-repeat w-full h-[800px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
-				/>
-				<div className='max-w-[1536px] relative mx-auto'>
-					<div className='flex flex-col items-center w-full justify-center relative overflow-y-hidden h-[calc(40vh-65px)] md:h-[calc(60vh-65px)] lg:h-[calc(100vh-65px)]'>
-						<div className='flex text-center md:text-left items-center md:items-start flex-col sm:pt-10 justify-start mx-auto gap-1 sm:gap-5 w-full  max-w-6xl'>
-							<h1
-								className={`text-2xl sm:text-4xl md:text-5xl animate-fade-in xl:text-[60px] 2xl:text-[70px] !leading-tight text-center font-bold ${space_grotesk.className}`}
-							>
-								Building a more intelligent future through the IQ token.
-							</h1>
-							<p className='md:text-lg max-w-[513px] animate-slide-up lg:max-w-2xl text-center mx-auto lg:text-xl'>
-								Join our mission to expand the scope of human knowledge with
-								BrainDAO, the governing DAO powering the IQ ecosystem.
-							</p>
-							<a
-								href={'#iq-token'}
-								className='mt-4 mx-auto hover:animate-bounce'
-							>
-								<RiArrowDownDoubleLine size={24} />
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div onMouseMove={manageMouseMove}>
+      <div className="relative bg-braindao-sm sm:bg-hero bg-cover bg-no-repeat bg-center text-whiteAlpha-900">
+        <Navbar />
+        <div
+          ref={plane}
+          className="hidden sm:block bg-brainie bg-cover bg-center bg-no-repeat w-full h-[800px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+        <div className="max-w-[1536px] relative mx-auto">
+          <div className="flex flex-col items-center w-full justify-center relative overflow-y-hidden h-[calc(40vh-65px)] md:h-[calc(60vh-65px)] lg:h-[calc(100vh-65px)]">
+            <div className="flex text-center md:text-left items-center md:items-start flex-col sm:pt-10 justify-start mx-auto gap-1 sm:gap-5 w-full  max-w-6xl">
+              <h1
+                className={`text-2xl sm:text-4xl md:text-5xl animate-fade-in xl:text-[60px] 2xl:text-[70px] !leading-tight text-center font-bold ${space_grotesk.className}`}
+              >
+                Building a more intelligent future through the IQ token.
+              </h1>
+              <p className="md:text-lg max-w-[513px] animate-slide-up lg:max-w-2xl text-center mx-auto lg:text-xl">
+                Join our mission to expand the scope of human knowledge with
+                BrainDAO, the governing DAO powering the IQ ecosystem.
+              </p>
+              <a
+                href={"#iq-token"}
+                className="mt-4 mx-auto hover:animate-bounce"
+              >
+                <RiArrowDownDoubleLine size={24} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/layouts/ecosystem/Iqwiki.tsx
+++ src/components/layouts/ecosystem/Iqwiki.tsx
@@ -1,86 +1,86 @@
-'use client';
+"use client";
 
-import InViewAnimateLeft from '@/components/transitions/InViewAnimateLeft';
-import InViewAnimateRight from '@/components/transitions/InViewAnimateRight';
-import InViewAnimateTop from '@/components/transitions/InViewAnimateTop';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { TokenBrief } from '../Iqtoken';
+import InViewAnimateLeft from "@/components/transitions/InViewAnimateLeft";
+import InViewAnimateRight from "@/components/transitions/InViewAnimateRight";
+import InViewAnimateTop from "@/components/transitions/InViewAnimateTop";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { TokenBrief } from "../Iqtoken";
 
 const Iqwiki = () => {
-	const { theme } = useTheme();
-	const [appTheme, setAppTheme] = useState('');
+  const { theme } = useTheme();
+  const [appTheme, setAppTheme] = useState("");
 
-	useEffect(() => {
-		setAppTheme(theme || '');
-	}, [theme]);
-	let imageUrl, imageUrl2, imageUrl3;
-	if (appTheme === 'light') {
-		imageUrl = '/images/iqwiki-widget.png';
-		imageUrl2 = '/images/iqwiki-rank.png';
-	}
-	if (appTheme === 'dark') {
-		imageUrl = '/images/iqwiki-widget-dark.png';
-		imageUrl2 = '/images/iqwiki-rank-dark.png';
-	}
+  useEffect(() => {
+    setAppTheme(theme || "");
+  }, [theme]);
+  let imageUrl, imageUrl2, imageUrl3;
+  if (appTheme === "light") {
+    imageUrl = "/images/iqwiki-widget.png";
+    imageUrl2 = "/images/iqwiki-rank.png";
+  }
+  if (appTheme === "dark") {
+    imageUrl = "/images/iqwiki-widget-dark.png";
+    imageUrl2 = "/images/iqwiki-rank-dark.png";
+  }
 
-	return (
-		<div id='iq-wiki'>
-			<TokenBrief
-				title='IQ Wiki'
-				description="IQ.wiki, the world's largest blockchain and cryptocurrency encyclopedia, is a Polygon-based platform, featuring thousands of pages, that uses IPFS for data storage and on-chain edits. IQ token stakers govern the platform, which offers a gasless experience for contributors and utilizes OpenAI's GPT-3 for article summarization."
-				action='https://iq.wiki'
-				buttonText='Explore IQ wiki'
-			/>
-			<div className='border overflow-hidden rounded-xl mt-16 border-gray200 dark:border-whiteAlpha-400 bg-white dark:bg-gray700 grid md:grid-cols-3'>
-				<div className='col-span-1 pl-3 pt-4 lg:pl-6 lg:pt-8'>
-					<p className='max-w-[390px] text-sm xl:text-base dark:text-whiteAlpha-800'>
-						Explore a diverse range of wikis designed to guide you through the
-						realms of cryptocurrency and blockchain.
-					</p>
-				</div>
-				<InViewAnimateTop className='col-span-2'>
-					<div className='h-[250px] sm:h-[300px] xl:h-[450px] bg-wiki-homepage bg-no-repeat dark:bg-wiki-homepage-dark bg-cover' />
-				</InViewAnimateTop>
-			</div>
-			<div className='flex flex-col md:flex-row gap-2 sm:gap-5 mt-2 sm:mt-5'>
-				<InViewAnimateLeft className='md:flex-1 rounded-xl relative overflow-hidden dark:bg-gray700 h-[330px] sm:h-[450px] md:h-[350px] lg:h-[470px] 2xl:h-[528px]'>
-					{imageUrl && (
-						<Image
-							src={imageUrl}
-							alt=''
-							fill
-							sizes='(max-width:480px) 100vw, 50vw'
-						/>
-					)}
-				</InViewAnimateLeft>
-				<InViewAnimateRight className='md:flex-1 flex flex-col gap-2 lg:gap-0 justify-between'>
-					<div className='w-full relative h-[300px] sm:h-[380px] md:h-[280px] lg:h-[380px] 2xl:h-[418px] rounded-xl dark:bg-gray700'>
-						{imageUrl2 && (
-							<Image
-								src={imageUrl2}
-								alt=''
-								fill
-								sizes='(max-width:480px) 100vw, 50vw'
-							/>
-						)}
-					</div>
-					<div className='p-3 lg:p-5 flex items-center bg-white dark:bg-gray700 border border-gray200 dark:border-whiteAlpha-400 justify-between gap-2 rounded-xl'>
-						<span className='font-medium md:text-sm lg:text-lg xl:text-xl dark:text-whiteAlpha-800'>
-							Integrations:
-						</span>
-						<div className='flex gap-3 lg:gap-6 items-center xl:gap-8'>
-							<div className='w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-eth bg-contain bg-no-repeat' />
-							<div className='w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-graph bg-contain bg-no-repeat' />
-							<div className='w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-coingecko bg-contain bg-no-repeat' />
-							<div className='2xl:w-32 2xl:h-12 md:w-20 md:h-8 w-24 h-10 lg:w-28 lg:h-10 bg-magic-white dark:bg-magic bg-contain bg-no-repeat' />
-						</div>
-					</div>
-				</InViewAnimateRight>
-			</div>
-		</div>
-	);
+  return (
+    <div id="iq-wiki">
+      <TokenBrief
+        title="IQ Wiki"
+        description="IQ.wiki, the world's largest blockchain and cryptocurrency encyclopedia, is a Polygon-based platform, featuring thousands of pages, that uses IPFS for data storage and on-chain edits. IQ token stakers govern the platform, which offers a gasless experience for contributors and utilizes OpenAI's GPT-3 for article summarization."
+        action="https://iq.wiki"
+        buttonText="Explore IQ wiki"
+      />
+      <div className="border overflow-hidden rounded-xl mt-16 border-gray200 dark:border-whiteAlpha-400 bg-white dark:bg-gray700 grid md:grid-cols-3">
+        <div className="col-span-1 pl-3 pt-4 lg:pl-6 lg:pt-8">
+          <p className="max-w-[390px] text-sm xl:text-base dark:text-whiteAlpha-800">
+            Explore a diverse range of wikis designed to guide you through the
+            realms of cryptocurrency and blockchain.
+          </p>
+        </div>
+        <InViewAnimateTop className="col-span-2">
+          <div className="h-[250px] sm:h-[300px] xl:h-[450px] bg-wiki-homepage bg-no-repeat dark:bg-wiki-homepage-dark bg-cover" />
+        </InViewAnimateTop>
+      </div>
+      <div className="flex flex-col md:flex-row gap-2 sm:gap-5 mt-2 sm:mt-5">
+        <InViewAnimateLeft className="md:flex-1 rounded-xl relative overflow-hidden dark:bg-gray700 h-[330px] sm:h-[450px] md:h-[350px] lg:h-[470px] 2xl:h-[528px]">
+          {imageUrl && (
+            <Image
+              src={imageUrl}
+              alt=""
+              fill
+              sizes="(max-width:480px) 100vw, 50vw"
+            />
+          )}
+        </InViewAnimateLeft>
+        <InViewAnimateRight className="md:flex-1 flex flex-col gap-2 lg:gap-0 justify-between">
+          <div className="w-full relative h-[300px] sm:h-[380px] md:h-[280px] lg:h-[380px] 2xl:h-[418px] rounded-xl dark:bg-gray700">
+            {imageUrl2 && (
+              <Image
+                src={imageUrl2}
+                alt=""
+                fill
+                sizes="(max-width:480px) 100vw, 50vw"
+              />
+            )}
+          </div>
+          <div className="p-3 lg:p-5 flex items-center bg-white dark:bg-gray700 border border-gray200 dark:border-whiteAlpha-400 justify-between gap-2 rounded-xl">
+            <span className="font-medium md:text-sm lg:text-lg xl:text-xl dark:text-whiteAlpha-800">
+              Integrations:
+            </span>
+            <div className="flex gap-3 lg:gap-6 items-center xl:gap-8">
+              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-eth bg-contain bg-no-repeat" />
+              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-graph bg-contain bg-no-repeat" />
+              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-coingecko bg-contain bg-no-repeat" />
+              <div className="2xl:w-32 2xl:h-12 md:w-20 md:h-8 w-24 h-10 lg:w-28 lg:h-10 bg-magic-white dark:bg-magic bg-contain bg-no-repeat" />
+            </div>
+          </div>
+        </InViewAnimateRight>
+      </div>
+    </div>
+  );
 };
 
 export default Iqwiki;
