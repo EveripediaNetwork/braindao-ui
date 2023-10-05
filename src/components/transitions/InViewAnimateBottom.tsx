a1b9e90880ab1fee1caa71968c797f3a2dcf77b3
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-05 14:08:31 +0100

    working on corrections

--- src/components/transitions/InViewAnimateBottom.tsx
+++ src/components/transitions/InViewAnimateBottom.tsx
@@ -1,47 +1,47 @@
-"use client";
+'use client';
 
-import { useAnimation, motion } from "framer-motion";
-import React, { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from 'framer-motion';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export type TInViewAnimate = {
-  children: React.ReactNode;
-  className?: string;
-  delay?: number;
+	children: React.ReactNode;
+	className?: string;
+	delay?: number;
 };
 const InViewAnimateBottom: React.FC<TInViewAnimate> = ({
-  children,
-  className,
-  delay,
+	children,
+	className,
+	delay,
 }) => {
-  const { ref, inView } = useInView();
-  const animation = useAnimation();
+	const { ref, inView } = useInView();
+	const animation = useAnimation();
 
-  useEffect(() => {
-    if (inView) {
-      animation.start({
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          duration: 1,
-          delay: delay ? delay : 0.1,
-        },
-      });
-    }
-    if (!inView) {
-      animation.start({
-        y: "10vw",
-        opacity: 0.5,
-      });
-    }
-  }, [animation, delay, inView]);
+	useEffect(() => {
+		if (inView) {
+			animation.start({
+				y: 0,
+				opacity: 1,
+				transition: {
+					type: 'tween',
+					duration: 1,
+					delay: delay ? delay : 0.6,
+				},
+			});
+		}
+		if (!inView) {
+			animation.start({
+				y: '10vw',
+				opacity: 0.5,
+			});
+		}
+	}, [animation, delay, inView]);
 
-  return (
-    <div ref={ref} className={`overflow-hidden ${className}`}>
-      <motion.div animate={animation}>{children}</motion.div>
-    </div>
-  );
+	return (
+		<div ref={ref} className={`overflow-hidden ${className}`}>
+			<motion.div animate={animation}>{children}</motion.div>
+		</div>
+	);
 };
 
 export default InViewAnimateBottom;
