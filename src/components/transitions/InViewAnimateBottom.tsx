636fe3f9f06e9d9585b8fea8fb918dbdab8a7f87
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-09 14:08:52 +0100

    further iterations on animation timing

--- src/components/transitions/InViewAnimateBottom.tsx
+++ src/components/transitions/InViewAnimateBottom.tsx
@@ -1,49 +1,49 @@
-"use client";
+'use client';
 
-import { useAnimation, motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export type TInViewAnimate = {
-  children: React.ReactNode;
-  className?: string;
-  delay?: number;
+	children: React.ReactNode;
+	className?: string;
+	delay?: number;
 };
 const InViewAnimateBottom: React.FC<TInViewAnimate> = ({
-  children,
-  className,
-  delay,
+	children,
+	className,
+	delay,
 }) => {
-  const { ref, inView } = useInView();
-  const animation = useAnimation();
-  const [hasAnimated, setHasAnimated] = useState(false);
+	const { ref, inView } = useInView();
+	const animation = useAnimation();
+	const [hasAnimated, setHasAnimated] = useState(false);
 
-  useEffect(() => {
-    if (inView && !hasAnimated) {
-      setHasAnimated(true);
-      animation.start({
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          duration: 0.4,
-          delay: delay ? delay : 0.2,
-        },
-      });
-    }
-    if (!inView && !hasAnimated) {
-      animation.start({
-        y: "10vw",
-        opacity: 0.5,
-      });
-    }
-  }, [animation, delay, inView, hasAnimated]);
+	useEffect(() => {
+		if (inView && !hasAnimated) {
+			setHasAnimated(true);
+			animation.start({
+				y: 0,
+				opacity: 1,
+				transition: {
+					type: 'tween',
+					duration: 0.8,
+					delay: delay ? delay : 0.2,
+				},
+			});
+		}
+		if (!inView && !hasAnimated) {
+			animation.start({
+				y: '10vw',
+				opacity: 0.5,
+			});
+		}
+	}, [animation, delay, inView, hasAnimated]);
 
-  return (
-    <div ref={ref} className={`overflow-hidden ${className}`}>
-      <motion.div animate={animation}>{children}</motion.div>
-    </div>
-  );
+	return (
+		<div ref={ref} className={`overflow-hidden ${className}`}>
+			<motion.div animate={animation}>{children}</motion.div>
+		</div>
+	);
 };
 
 export default InViewAnimateBottom;
