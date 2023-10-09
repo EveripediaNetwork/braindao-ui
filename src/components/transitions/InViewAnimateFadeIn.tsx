3ddae36594d0ad76bfa8dda0f8d019eba7277ed9
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-09 14:09:24 +0100

    biome formatting

--- src/components/transitions/InViewAnimateFadeIn.tsx
+++ src/components/transitions/InViewAnimateFadeIn.tsx
@@ -1,47 +1,47 @@
-'use client';
+"use client";
 
-import { useAnimation, motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useAnimation, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export type TInViewAnimate = {
-	children: React.ReactNode;
-	className?: string;
-	delay?: number;
+  children: React.ReactNode;
+  className?: string;
+  delay?: number;
 };
 const InViewAnimateFadeIn: React.FC<TInViewAnimate> = ({
-	children,
-	className,
-	delay,
+  children,
+  className,
+  delay,
 }) => {
-	const { ref, inView } = useInView();
-	const animation = useAnimation();
-	const [hasAnimated, setHasAnimated] = useState(false);
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+  const [hasAnimated, setHasAnimated] = useState(false);
 
-	useEffect(() => {
-		if (inView && !hasAnimated) {
-			setHasAnimated(true);
-			animation.start({
-				opacity: 1,
-				transition: {
-					duration: 0.8,
-					delay: delay ? delay : 0.2,
-					ease: 'easeInOut',
-				},
-			});
-		}
-		if (!inView && !hasAnimated) {
-			animation.start({
-				opacity: 0,
-			});
-		}
-	}, [inView, animation, delay, hasAnimated]);
+  useEffect(() => {
+    if (inView && !hasAnimated) {
+      setHasAnimated(true);
+      animation.start({
+        opacity: 1,
+        transition: {
+          duration: 0.8,
+          delay: delay ? delay : 0.2,
+          ease: "easeInOut",
+        },
+      });
+    }
+    if (!inView && !hasAnimated) {
+      animation.start({
+        opacity: 0,
+      });
+    }
+  }, [inView, animation, delay, hasAnimated]);
 
-	return (
-		<motion.div ref={ref} animate={animation} className={`${className}`}>
-			{children}
-		</motion.div>
-	);
+  return (
+    <motion.div ref={ref} animate={animation} className={`${className}`}>
+      {children}
+    </motion.div>
+  );
 };
 
 export default InViewAnimateFadeIn;
