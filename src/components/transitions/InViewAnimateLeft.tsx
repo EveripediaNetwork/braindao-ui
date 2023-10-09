b6b4d8d1fb25504f993a0a5baeafc9ae4bb73aec
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-09 14:26:12 +0100

    working on general animation time

--- src/components/transitions/InViewAnimateLeft.tsx
+++ src/components/transitions/InViewAnimateLeft.tsx
@@ -1,45 +1,45 @@
-"use client";
+'use client';
 
-import { useAnimation, motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
-import { TInViewAnimate } from "./InViewAnimateBottom";
+import { useAnimation, motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { TInViewAnimate } from './InViewAnimateBottom';
 
 const InViewAnimateLeft: React.FC<TInViewAnimate> = ({
-  children,
-  className,
-  delay,
+	children,
+	className,
+	delay,
 }) => {
-  const { ref, inView } = useInView();
-  const animation = useAnimation();
-  const [hasAnimated, setHasAnimated] = useState(false);
+	const { ref, inView } = useInView();
+	const animation = useAnimation();
+	const [hasAnimated, setHasAnimated] = useState(false);
 
-  useEffect(() => {
-    if (inView && !hasAnimated) {
-      setHasAnimated(true);
-      animation.start({
-        x: 0,
-        opacity: 1.0,
-        transition: {
-          type: "tween",
-          duration: 0.8,
-          delay: delay ? delay : 0.2,
-        },
-      });
-    }
-    if (!inView && !hasAnimated) {
-      animation.start({
-        x: "-10vw",
-        opacity: 0.5,
-      });
-    }
-  }, [animation, delay, inView, hasAnimated]);
+	useEffect(() => {
+		if (inView && !hasAnimated) {
+			setHasAnimated(true);
+			animation.start({
+				x: 0,
+				opacity: 1.0,
+				transition: {
+					type: 'tween',
+					duration: 0.8,
+					delay: delay ? delay : 0.2,
+				},
+			});
+		}
+		if (!inView && !hasAnimated) {
+			animation.start({
+				x: '-10vw',
+				opacity: 0.5,
+			});
+		}
+	}, [animation, delay, inView, hasAnimated]);
 
-  return (
-    <motion.div ref={ref} animate={animation} className={`${className}`}>
-      {children}
-    </motion.div>
-  );
+	return (
+		<motion.div ref={ref} animate={animation} className={`${className}`}>
+			{children}
+		</motion.div>
+	);
 };
 
 export default InViewAnimateLeft;
