f3ed1e6f4f558a395eeb99bc71157bc7d6018f9f
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-09 21:21:33 +0100

    biome formatting

--- src/components/transitions/inViewSlideUp.tsx
+++ src/components/transitions/inViewSlideUp.tsx
@@ -1,46 +1,46 @@
-'use client';
+"use client";
 
-import { useAnimation, motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useAnimation, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export type TInViewAnimate = {
-	children: React.ReactNode;
-	className?: string;
-	delay?: number;
+  children: React.ReactNode;
+  className?: string;
+  delay?: number;
 };
 const InViewAnimateSlideUp: React.FC<TInViewAnimate> = ({
-	children,
-	className,
-	delay,
+  children,
+  className,
+  delay,
 }) => {
-	const { ref, inView } = useInView();
-	const animation = useAnimation();
-	const [hasAnimated, setHasAnimated] = useState(false);
+  const { ref, inView } = useInView();
+  const animation = useAnimation();
+  const [hasAnimated, setHasAnimated] = useState(false);
 
-	useEffect(() => {
-		if (inView && !hasAnimated) {
-			setHasAnimated(true);
-			animation.start({
-				y: '-400px',
-				transition: {
-					type: 'tween',
-					duration: 1.5,
-				},
-			});
-		}
-		if (!inView && !hasAnimated) {
-			animation.start({
-				y: 0,
-			});
-		}
-	}, [animation, delay, inView, hasAnimated]);
+  useEffect(() => {
+    if (inView && !hasAnimated) {
+      setHasAnimated(true);
+      animation.start({
+        y: "-400px",
+        transition: {
+          type: "tween",
+          duration: 1.5,
+        },
+      });
+    }
+    if (!inView && !hasAnimated) {
+      animation.start({
+        y: 0,
+      });
+    }
+  }, [animation, delay, inView, hasAnimated]);
 
-	return (
-		<motion.div animate={animation} ref={ref} className={`${className}`}>
-			{children}
-		</motion.div>
-	);
+  return (
+    <motion.div animate={animation} ref={ref} className={`${className}`}>
+      {children}
+    </motion.div>
+  );
 };
 
 export default InViewAnimateSlideUp;
