97619fcb2d0f3e2b635eda5db1665208cbffce03
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-10 13:13:35 +0100

    further iterations on page content

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,35 +1,32 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div className="pt-16 pb-12 bg-brand-200">
-      <div className="p-8 flex flex-col items-center gap-10 border-b border-blackAlpha-50">
-        <Image src={"/svgs/brainie.svg"} alt="" width={40} height={34} />
-        <div className="flex flex-col items-center gap-4">
-          <h2 className="text-brand-800 text-center">
-            Stay up to date every month with our newsletter
-          </h2>
-          <p className="text-gray800 text-center">
-            Never miss any of the most popular and trending articles on IQ.wiki
-            when you sign up to our email newsletter.
-          </p>
-          <a
-            href={"https://www.getdrip.com/forms/505929689/submissions/new"}
-            target="_blank"
-            className="flex justify-center w-[210px] text-white/90 bg-brand-500 py-3 rounded-md hover:bg-brand-700 transition-all ease-in-out duration-300"
-          >
-            Subscribe
-          </a>
-        </div>
-      </div>
-      <div className="pt-8 px-8 text-center">
-        <small className="pt-5 text-gray800">
-          ©️2023 Powered by IQ and BrainDAO. All Rights Reserved.
-        </small>
-      </div>
-    </div>
-  );
+	return (
+		<div className='pt-16 pb-12 bg-brand-200'>
+			<div className='p-8 flex flex-col items-center gap-5 border-b border-blackAlpha-50'>
+				<Image src={'/svgs/brainie.svg'} alt='' width={40} height={34} />
+				<div className='flex flex-col items-center gap-4'>
+					<p className='text-gray800 text-center'>
+						Never miss BrainDAO & IQ ecosystem updates by signing up for our
+						email newsletter.
+					</p>
+					<a
+						href={'https://www.getdrip.com/forms/505929689/submissions/new'}
+						target='_blank'
+						className='flex justify-center w-[210px] text-white/90 bg-brand-500 py-3 rounded-md hover:bg-brand-700 transition-all ease-in-out duration-300'
+					>
+						Subscribe
+					</a>
+				</div>
+			</div>
+			<div className='pt-8 px-8 text-center'>
+				<small className='pt-5 text-gray800'>
+					©️2023 Powered by IQ and BrainDAO. All Rights Reserved.
+				</small>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- src/components/layouts/ecosystem/IqDashboard.tsx
+++ src/components/layouts/ecosystem/IqDashboard.tsx
@@ -1,54 +1,53 @@
-import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
-import Image from "next/image";
-import React from "react";
-import { StatsPointers, TokenBrief } from "../Iqtoken";
+import InViewAnimateBottom from '@/components/transitions/InViewAnimateBottom';
+import Image from 'next/image';
+import React from 'react';
+import { StatsPointers, TokenBrief } from '../Iqtoken';
 
 const IqDashboard = () => {
-  return (
-    <div className="max-w-[1536px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24">
-      <div className="px-10 xl:px-[120px]">
-        <TokenBrief
-          title="IQ Dashboard"
-          description="Utilizing the IQ Dashboard, you have the ability to stake your tokens, facilitate cross-blockchain bridging, participate in governance proposal voting, and engage in numerous other activities within the blockchain ecosystem."
-          action="https://iq.braindao.org/dashboard"
-          buttonText="Explore IQ Dashboard"
-        />
-        <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-12 xl:gap-6 py-3 mt-16 w-full">
-          <StatsPointers
-            title="Swap"
-            headerSize="text-xl"
-            content="Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms."
-            className=" gap-4"
-          />
-          <StatsPointers
-            title="Bridge"
-            headerSize="text-xl"
-            content="Transfer IQ from EOS to ETH and vice versa using the IQ bridge
-"
-            className="gap-4"
-          />
-          <StatsPointers
-            title="Bridge"
-            headerSize="text-xl"
-            content="Lock IQ token over a period of time and earn IQ token rewards.
-"
-            className="gap-4"
-          />
-          <StatsPointers
-            title="Voting"
-            headerSize="text-xl"
-            content="Stakers can vote on all governance proposals and create their own proposals."
-            className="gap-4"
-          />
-        </div>
-      </div>
-      <InViewAnimateBottom>
-        <div className="w-full relative h-[300px] sm:h-[500px] md:h-[600px] xl:h-[887px]">
-          <Image src={"/images/dashboard-new.png"} alt="" fill sizes="100vw" />
-        </div>
-      </InViewAnimateBottom>
-    </div>
-  );
+	return (
+		<div className='max-w-[1536px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24'>
+			<div className='px-10 xl:px-[120px]'>
+				<TokenBrief
+					title='IQ Dashboard'
+					description='Using the IQ Dashboard, you can stake your tokens, facilitate cross-blockchain bridging, vote on governance proposals, and engage in many other activities within the blockchain ecosystem.'
+					action='https://iq.braindao.org/dashboard'
+					buttonText='Explore IQ Dashboard'
+				/>
+				<div className='grid sm:grid-cols-2 xl:grid-cols-4 gap-12 xl:gap-6 py-3 mt-16 w-full'>
+					<StatsPointers
+						title='Swap'
+						headerSize='text-xl'
+						content='Engage with the IQ ecosystem and effortlessly swap the IQ token across various exchange platforms'
+						className=' gap-4'
+					/>
+					<StatsPointers
+						title='Bridge'
+						headerSize='text-xl'
+						content='Transfer IQ from EOS to ETH and vice versa using the IQ bridge.'
+						className='gap-4'
+					/>
+					<StatsPointers
+						title='Lock'
+						headerSize='text-xl'
+						content='Lock IQ token over a period of time and earn IQ token rewards.
+'
+						className='gap-4'
+					/>
+					<StatsPointers
+						title='Vote'
+						headerSize='text-xl'
+						content='Stakers can vote on all governance proposals and create their own proposals.'
+						className='gap-4'
+					/>
+				</div>
+			</div>
+			<InViewAnimateBottom>
+				<div className='w-full relative h-[300px] sm:h-[500px] md:h-[600px] xl:h-[887px]'>
+					<Image src={'/images/dashboard-new.png'} alt='' fill sizes='100vw' />
+				</div>
+			</InViewAnimateBottom>
+		</div>
+	);
 };
 
 export default IqDashboard;
