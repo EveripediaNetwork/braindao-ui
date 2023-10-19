d0440d018e41a962fc1e1f66907729e54974d540
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-19 18:09:27 +0100

    biome formating

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,16 +1,16 @@
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect';
-import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask';
-import { mainnet } from 'wagmi';
-import { configureChains } from '@wagmi/core';
-import { goerli } from 'wagmi/chains';
-import { alchemyProvider } from '@wagmi/core/providers/alchemy';
-import { publicProvider } from '@wagmi/core/providers/public';
+import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect";
+import { MetaMaskConnector } from "@wagmi/core/connectors/metaMask";
+import { mainnet } from "wagmi";
+import { configureChains } from "@wagmi/core";
+import { goerli } from "wagmi/chains";
+import { alchemyProvider } from "@wagmi/core/providers/alchemy";
+import { publicProvider } from "@wagmi/core/providers/public";
 
-import config from './index';
+import config from "./index";
 
-const chainArray = config.alchemyChain === 'goerli' ? goerli : mainnet;
+const chainArray = config.alchemyChain === "goerli" ? goerli : mainnet;
 
 export const { chains, publicClient } = configureChains(
-	[chainArray],
-	[alchemyProvider({ apiKey: config.alchemyApiKey }), publicProvider()]
+  [chainArray],
+  [alchemyProvider({ apiKey: config.alchemyApiKey }), publicProvider()]
 );
