129f92dde491d5275b50a7c6082babbc513a28fa
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-23 08:05:40 +0100

    reworked the default theme implementation

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,35 +1,35 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div className="pt-16 pb-12 bg-brand-200 dark:bg-brand-50">
-      <div className="p-8 flex flex-col items-center gap-5 border-b border-blackAlpha-50">
-        <Image src={"/svgs/brainie.svg"} alt="" width={40} height={34} />
-        <div className="flex flex-col items-center gap-4">
-          <h2 className="dark:text-brand-500 text-brand-800 text-xl md:text-2xl xl:text-3xl font-bold text-center">
-            Stay up to date every month with our newsletter
-          </h2>
-          <p className="text-gray800 text-center text-sm sm:text-base">
-            Never miss BrainDAO & IQ ecosystem updates by signing up for our
-            email newsletter.
-          </p>
-          <a
-            href={"https://www.getdrip.com/forms/505929689/submissions/new"}
-            target="_blank"
-            className="flex justify-center w-[210px] text-white bg-brand-800 dark:bg-brand-500 py-3 rounded-md hover:bg-brand-700 transition-all ease-in-out duration-300"
-          >
-            Subscribe
-          </a>
-        </div>
-      </div>
-      <div className="pt-8 px-8 text-center">
-        <small className="pt-5 text-xs text-gray800 dark:text-gray600">
-          ©️2023 Powered by IQ and BrainDAO. All Rights Reserved.
-        </small>
-      </div>
-    </div>
-  );
+	return (
+		<div className='pt-16 pb-12 dark:bg-brand-200 bg-brand-50'>
+			<div className='p-8 flex flex-col items-center gap-5 border-b border-blackAlpha-50'>
+				<Image src={'/svgs/brainie.svg'} alt='' width={40} height={34} />
+				<div className='flex flex-col items-center gap-4'>
+					<h2 className='text-brand-500 dark:text-brand-800 text-xl md:text-2xl xl:text-3xl font-bold text-center'>
+						Stay up to date every month with our newsletter
+					</h2>
+					<p className='text-gray800 text-center text-sm sm:text-base'>
+						Never miss BrainDAO & IQ ecosystem updates by signing up for our
+						email newsletter.
+					</p>
+					<a
+						href={'https://www.getdrip.com/forms/505929689/submissions/new'}
+						target='_blank'
+						className='flex justify-center w-[210px] text-white dark:bg-brand-800 bg-brand-500 py-3 rounded-md hover:bg-brand-700 transition-all ease-in-out duration-300'
+					>
+						Subscribe
+					</a>
+				</div>
+			</div>
+			<div className='pt-8 px-8 text-center'>
+				<small className='pt-5 text-xs dark:text-gray800 text-gray600'>
+					©️2023 Powered by IQ and BrainDAO. All Rights Reserved.
+				</small>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- src/components/layouts/theme/ThemeProvider.tsx
+++ src/components/layouts/theme/ThemeProvider.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { PropsWithChildren } from "react";
-import { ThemeProvider } from "next-themes";
-import { createConfig, WagmiConfig } from "wagmi";
-import { publicClient } from "@/config/wagmi";
+import { PropsWithChildren } from 'react';
+import { ThemeProvider } from 'next-themes';
+import { createConfig, WagmiConfig } from 'wagmi';
+import { publicClient } from '@/config/wagmi';
 
 const client = createConfig({
-  autoConnect: true,
-  publicClient,
+	autoConnect: true,
+	publicClient,
 });
 
 const ClientProviders = ({ children }: PropsWithChildren) => {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      <WagmiConfig config={client}>{children}</WagmiConfig>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider attribute='class' defaultTheme='dark'>
+			<WagmiConfig config={client}>{children}</WagmiConfig>
+		</ThemeProvider>
+	);
 };
 
 export default ClientProviders;
