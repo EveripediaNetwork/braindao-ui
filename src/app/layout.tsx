defc9f509cc005dd087b9f01c919126f7028ea08
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-12-07 16:02:30 +0100

    further iterations on fade-in animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,68 +1,66 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { montserrat } from "./font";
-import ClientProviders from "@/components/layouts/theme/ThemeProvider";
-import Footer from "@/components/layouts/Footer";
-import Navbar from "@/components/layouts/Navbar";
+import './globals.css';
+import type { Metadata } from 'next';
+import { montserrat } from './font';
+import ClientProviders from '@/components/layouts/theme/ThemeProvider';
+import Footer from '@/components/layouts/Footer';
 
 export const metadata: Metadata = {
-  title: "BrainDAO - Building a more intelligent future through the IQ token.",
-  description:
-    "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
-  viewport: "",
-  openGraph: {
-    title:
-      "BrainDAO - Building a more intelligent future through the IQ token.",
-    url: "https://braindao.org",
-    type: "website",
-    description:
-      "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
-    images: [
-      {
-        url: "https://braindao.org/images/og-image.png",
-        alt: "BrainDAO - Building a more intelligent future through the IQ token.",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title:
-      "BrainDAO - Building a more intelligent future through the IQ token.",
-    description:
-      "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
-    images: ["https://braindao.org/images/og-image.png"],
-    site: "@Everipedia",
-    creator: "@Everipedia",
-  },
+	title: 'BrainDAO - Building a more intelligent future through the IQ token.',
+	description:
+		'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
+	viewport: '',
+	openGraph: {
+		title:
+			'BrainDAO - Building a more intelligent future through the IQ token.',
+		url: 'https://braindao.org',
+		type: 'website',
+		description:
+			'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
+		images: [
+			{
+				url: 'https://braindao.org/images/og-image.png',
+				alt: 'BrainDAO - Building a more intelligent future through the IQ token.',
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title:
+			'BrainDAO - Building a more intelligent future through the IQ token.',
+		description:
+			'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
+		images: ['https://braindao.org/images/og-image.png'],
+		site: '@Everipedia',
+		creator: '@Everipedia',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={montserrat.className}>
-      <head>
-        <link rel="canonical" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-      </head>
-      <body className="">
-        <ClientProviders>
-          <div className="overflow-x-hidden">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </ClientProviders>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' className={montserrat.className}>
+			<head>
+				<link rel='canonical' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<link rel='icon' href='/favicon.ico' type='image/x-icon' />
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin=''
+				/>
+			</head>
+			<body className=''>
+				<ClientProviders>
+					<div className='overflow-x-hidden'>
+						{children}
+						<Footer />
+					</div>
+				</ClientProviders>
+			</body>
+		</html>
+	);
 }
--- src/components/transitions/InViewAnimateBottom.tsx
+++ src/components/transitions/InViewAnimateBottom.tsx
@@ -1,48 +1,53 @@
-"use client";
+'use client';
 
-import { useAnimation, motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export type TInViewAnimate = {
-  children: React.ReactNode;
-  className?: string;
-  delay?: number;
+	children: React.ReactNode;
+	className?: string;
+	delay?: number;
 };
 const InViewAnimateBottom: React.FC<TInViewAnimate> = ({
-  children,
-  className,
-  delay,
+	children,
+	className,
+	delay,
 }) => {
-  const { ref, inView } = useInView();
-  const animation = useAnimation();
-  const [hasAnimated, setHasAnimated] = useState(false);
+	const { ref, inView } = useInView();
+	const animation = useAnimation();
+	const [hasAnimated, setHasAnimated] = useState(false);
 
-  useEffect(() => {
-    if (inView && !hasAnimated) {
-      setHasAnimated(true);
-      animation.start({
-        // y: 0,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          duration: 1.5,
-        },
-      });
-    }
-    if (!inView && !hasAnimated) {
-      animation.start({
-        // y: "5vw",
-        opacity: 0.2,
-      });
-    }
-  }, [animation, delay, inView, hasAnimated]);
+	useEffect(() => {
+		if (inView && !hasAnimated) {
+			setHasAnimated(true);
+			animation.start({
+				// y: 0,
+				opacity: 1,
+				transition: {
+					type: 'tween',
+					duration: 1.5,
+				},
+			});
+		}
+		if (!inView && !hasAnimated) {
+			animation.start({
+				// y: '100px',
+				opacity: 0,
+			});
+		}
+	}, [animation, delay, inView, hasAnimated]);
 
-  return (
-    <div ref={ref} className={`${className}`}>
-      <motion.div animate={animation}>{children}</motion.div>
-    </div>
-  );
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			viewport={{ once: true }}
+			transition={{ duration: 1.5 }}
+		>
+			{children}
+		</motion.div>
+	);
 };
 
 export default InViewAnimateBottom;
