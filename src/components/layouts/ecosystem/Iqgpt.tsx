021e2786f663bd603ac1e81293e9e4c049a17f10
Adebesin Cell <adebesintolulope80@gmail.com>
2024-04-04 08:59:15 +0100

    Fixes IQ CODE and IQ Social sections - formatting using biome

--- src/components/layouts/ecosystem/Iqgpt.tsx
+++ src/components/layouts/ecosystem/Iqgpt.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import InViewAnimateBottom from '@/components/transitions/InViewAnimateBottom';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import {
   RiDatabaseLine,
   RiNewspaperLine,
   RiPlug2Line,
-  RiQuestionAnswerLine
-} from 'react-icons/ri';
-import { TokenBrief } from '../Iqtoken';
+  RiQuestionAnswerLine,
+} from "react-icons/ri";
+import { TokenBrief } from "../Iqtoken";
 
 export const FeatureList = ({
   title,
   description,
   icon,
-  delay
+  delay,
 }: {
   title: string;
   description: string;
@@ -24,15 +24,15 @@ export const FeatureList = ({
   delay?: number;
 }) => {
   return (
-    <InViewAnimateBottom className='flex flex-1 flex-col gap-4 items-start'>
-      <div className='w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100'>
+    <InViewAnimateBottom className="flex flex-1 flex-col gap-4 items-start">
+      <div className="w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100">
         {icon}
       </div>
-      <div className=''>
-        <h4 className='dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1'>
+      <div className="">
+        <h4 className="dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1">
           {title}
         </h4>
-        <p className='text-sm lg:text-base'>{description}</p>
+        <p className="text-sm lg:text-base">{description}</p>
       </div>
     </InViewAnimateBottom>
   );
@@ -40,64 +40,64 @@ export const FeatureList = ({
 
 const Iqgpt = () => {
   const { theme } = useTheme();
-  const [appTheme, setAppTheme] = useState('');
+  const [appTheme, setAppTheme] = useState("");
 
   useEffect(() => {
-    setAppTheme(theme ?? '');
+    setAppTheme(theme ?? "");
   }, [theme]);
   let imageUrl: string | undefined;
-  if (appTheme === 'light') {
-    imageUrl = '/images/iq-gpt.png';
+  if (appTheme === "light") {
+    imageUrl = "/images/iq-gpt.png";
   }
-  if (appTheme === 'dark') {
-    imageUrl = '/images/iq-gpt-dark.png';
+  if (appTheme === "dark") {
+    imageUrl = "/images/iq-gpt-dark.png";
   }
 
   return (
     <div
-      id='iq-gpt'
-      className='max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative'
+      id="iq-gpt"
+      className="max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative"
     >
       <TokenBrief
-        title='IQ GPT'
+        title="IQ GPT"
         description={`IQ GPT is an extension of IQ.wiki's decentralized framework, integrating crypto-focused AI models that utilize diverse data sources to bolster capabilities, offering insights into intricate terms, live market trends, and breaking news.`}
         description2={
-          'IQ GPT is powered by the IQ token, enabling IQ holders to participate in governance and get access to additional features.'
+          "IQ GPT is powered by the IQ token, enabling IQ holders to participate in governance and get access to additional features."
         }
-        action='https://iqgpt.com'
-        buttonText='Explore IQ GPT'
+        action="https://iqgpt.com"
+        buttonText="Explore IQ GPT"
       />
-      <div className='grid min-[500px]:grid-cols-2 sm:grid-cols-2 xl:grid-cols-4 gap-4 2xl:gap-12 py-2'>
+      <div className="grid min-[500px]:grid-cols-2 sm:grid-cols-2 xl:grid-cols-4 gap-4 2xl:gap-12 py-2">
         <FeatureList
           icon={<RiDatabaseLine />}
-          title='Access to real-time data'
-          description='Access to real-time price data, trading volumes, and market capitalization.'
+          title="Access to real-time data"
+          description="Access to real-time price data, trading volumes, and market capitalization."
         />
         <FeatureList
           icon={<RiQuestionAnswerLine />}
-          title='AI-generated answers'
-          description='Receive AI-generated answers to all your crypto questions.'
+          title="AI-generated answers"
+          description="Receive AI-generated answers to all your crypto questions."
           delay={0.8}
         />
         <FeatureList
           icon={<RiNewspaperLine />}
-          title='Update on news and trends'
-          description='Equip yourself with AI-powered market analysis using real-time market data, news, and trends.'
+          title="Update on news and trends"
+          description="Equip yourself with AI-powered market analysis using real-time market data, news, and trends."
           delay={1.0}
         />
         <FeatureList
           icon={<RiPlug2Line />}
-          title='Plugins'
-          description='Access various plugins to select data sources for your inquiries.'
+          title="Plugins"
+          description="Access various plugins to select data sources for your inquiries."
           delay={1.2}
         />
       </div>
       <InViewAnimateBottom>
-        <div className='w-full relative h-[320px] min-[500px]:h-[450px] sm:h-[600px] md:h-[650px] lg:h-[850px] xl:h-[1000px] 2xl:h-[1117px]'>
-          {imageUrl && <Image src={imageUrl} alt='' fill sizes='100vw' />}
+        <div className="w-full relative h-[320px] min-[500px]:h-[450px] sm:h-[600px] md:h-[650px] lg:h-[850px] xl:h-[1000px] 2xl:h-[1117px]">
+          {imageUrl && <Image src={imageUrl} alt="" fill sizes="100vw" />}
         </div>
       </InViewAnimateBottom>
-      <div className='h-[174px] w-full absolute dark:bg-gradient-linear bg-gradient-linear-light bg-cover left-0 -bottom-2 sm:bottom-0' />
+      <div className="h-[174px] w-full absolute dark:bg-gradient-linear bg-gradient-linear-light bg-cover left-0 -bottom-2 sm:bottom-0" />
     </div>
   );
 };
--- src/components/layouts/ecosystem/iqcode.tsx
+++ src/components/layouts/ecosystem/iqcode.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import InViewAnimateBottom from '@/components/transitions/InViewAnimateBottom';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { TokenBrief } from '../Iqtoken';
+import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { TokenBrief } from "../Iqtoken";
 
 export const FeatureList = ({
   title,
   description,
   icon,
-  delay
+  delay,
 }: {
   title: string;
   description: string;
@@ -18,15 +18,15 @@ export const FeatureList = ({
   delay?: number;
 }) => {
   return (
-    <InViewAnimateBottom className='flex flex-1 flex-col gap-4 items-start'>
-      <div className='w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100'>
+    <InViewAnimateBottom className="flex flex-1 flex-col gap-4 items-start">
+      <div className="w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100">
         {icon}
       </div>
-      <div className=''>
-        <h4 className='dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1'>
+      <div className="">
+        <h4 className="dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1">
           {title}
         </h4>
-        <p className='text-sm lg:text-base'>{description}</p>
+        <p className="text-sm lg:text-base">{description}</p>
       </div>
     </InViewAnimateBottom>
   );
@@ -34,48 +34,48 @@ export const FeatureList = ({
 
 const Iqcode = () => {
   const { theme } = useTheme();
-  const [appTheme, setAppTheme] = useState('');
+  const [appTheme, setAppTheme] = useState("");
 
   useEffect(() => {
-    setAppTheme(theme ?? '');
+    setAppTheme(theme ?? "");
   }, [theme]);
 
   let imageUrl: string | undefined;
-  if (appTheme === 'light') {
-    imageUrl = '/images/iq-code-light.png';
+  if (appTheme === "light") {
+    imageUrl = "/images/iq-code-light.png";
   }
-  if (appTheme === 'dark') {
-    imageUrl = '/images/iq-code-dark.png';
+  if (appTheme === "dark") {
+    imageUrl = "/images/iq-code-dark.png";
   }
 
   return (
     <div
-      id='iq-code'
-      className='max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative'
+      id="iq-code"
+      className="max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative"
     >
       <TokenBrief
-        title='IQ code'
+        title="IQ code"
         description={
-          'IQ code is an upcoming smart contract coding tool with AI features specifically tailored for smart contracts. The product will function as a Solidity advisor, aiming to improve coding efficiency and code quality, to assist developers. '
+          "IQ code is an upcoming smart contract coding tool with AI features specifically tailored for smart contracts. The product will function as a Solidity advisor, aiming to improve coding efficiency and code quality, to assist developers. "
         }
-        action='https://iqcode.ai/demo'
-        buttonText='See Demo'
+        action="https://iqcode.ai/demo"
+        buttonText="See Demo"
       />
 
       <InViewAnimateBottom>
-        <div className='w-full relative h-[320px] min-[500px]:h-[450px] sm:h-[600px] md:h-[650px] lg:h-[750px] xl:h-[1000px]'>
+        <div className="w-full relative h-[320px] min-[500px]:h-[450px] sm:h-[600px] md:h-[650px] lg:h-[750px] xl:h-[1000px]">
           {imageUrl && (
             <Image
               src={imageUrl}
-              alt='IQ Code Illustration'
+              alt="IQ Code Illustration"
               fill
-              sizes='100vw'
-              className='object-cover md:object-contain'
+              sizes="100vw"
+              className="object-cover md:object-contain"
             />
           )}
         </div>
       </InViewAnimateBottom>
-      <div className='h-[174px] w-full absolute dark:bg-gradient-linear bg-gradient-linear-light bg-cover left-0 -bottom-2 sm:bottom-0' />
+      <div className="h-[174px] w-full absolute dark:bg-gradient-linear bg-gradient-linear-light bg-cover left-0 -bottom-2 sm:bottom-0" />
     </div>
   );
 };
