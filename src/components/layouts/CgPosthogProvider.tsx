61efced8901d6c8335a317f881eef6fa1aaaa8a7
Adebesin Cell <adebesintolulope80@gmail.com>
2024-06-21 21:47:21 +0100

    adds autocapture and fixes formatting

--- src/components/layouts/CgPosthogProvider.tsx
+++ src/components/layouts/CgPosthogProvider.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import config from '@/config';
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import config from "@/config";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   posthog.init(config.posthogApiKey, {
-    api_host: '/ingest',
-    ui_host: 'https://us.posthog.com',
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
     autocapture: {
-      dom_event_allowlist: ['click'],
-      element_allowlist: ['button', 'a']
-    }
+      dom_event_allowlist: ["click"],
+      element_allowlist: ["button", "a"],
+    },
   });
 }
 
