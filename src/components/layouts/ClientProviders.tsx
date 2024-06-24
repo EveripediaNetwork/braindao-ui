e7afbbd423e20f867465297a8e55060f59f47833
Adebesin Cell <adebesintolulope80@gmail.com>
2024-06-24 12:02:47 +0100

    fixes files

--- src/components/layouts/ClientProviders.tsx
+++ src/components/layouts/ClientProviders.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { publicClient } from '@/config/wagmi';
-import { ThemeProvider } from 'next-themes';
-import type { PropsWithChildren } from 'react';
-import { WagmiConfig, createConfig } from 'wagmi';
-import { CSPostHogProvider } from './CgPosthogProvider';
+import { publicClient } from "@/config/wagmi";
+import { ThemeProvider } from "next-themes";
+import type { PropsWithChildren } from "react";
+import { WagmiConfig, createConfig } from "wagmi";
+import { CSPostHogProvider } from "./CgPosthogProvider";
 
 const client = createConfig({
   autoConnect: true,
-  publicClient
+  publicClient,
 });
 
 const ClientProviders = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider attribute='class' defaultTheme='dark'>
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <WagmiConfig config={client}>
         <CSPostHogProvider>{children}</CSPostHogProvider>
       </WagmiConfig>
