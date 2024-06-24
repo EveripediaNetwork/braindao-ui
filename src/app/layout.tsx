fb93acaa7b77d1460b3047e768265f31f7d34386
Adebesin Cell <adebesintolulope80@gmail.com>
2024-06-24 12:02:38 +0100

    fixes files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,62 +1,62 @@
-import "./globals.css";
-import Footer from "@/components/layouts/Footer";
-import ClientProviders from "@/components/layouts/theme/ThemeProvider";
-import type { Metadata } from "next";
-import { montserrat } from "./font";
+import './globals.css';
+import ClientProviders from '@/components/layouts/ClientProviders';
+import Footer from '@/components/layouts/Footer';
+import type { Metadata } from 'next';
+import { montserrat } from './font';
 
 export const metadata: Metadata = {
-  title: "BrainDAO - Building a more intelligent future through the IQ token.",
+  title: 'BrainDAO - Building a more intelligent future through the IQ token.',
   description:
-    "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
-  viewport: "",
-  metadataBase: new URL("https://braindao.org"),
+    'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
+  viewport: '',
+  metadataBase: new URL('https://braindao.org'),
   openGraph: {
     title:
-      "BrainDAO - Building a more intelligent future through the IQ token.",
-    url: "https://braindao.org",
-    type: "website",
+      'BrainDAO - Building a more intelligent future through the IQ token.',
+    url: 'https://braindao.org',
+    type: 'website',
     description:
-      "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
+      'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
     images: [
       {
-        url: "https://braindao.org/images/og-image.png",
-        alt: "BrainDAO - Building a more intelligent future through the IQ token.",
-      },
-    ],
+        url: 'https://braindao.org/images/og-image.png',
+        alt: 'BrainDAO - Building a more intelligent future through the IQ token.'
+      }
+    ]
   },
   twitter: {
-    card: "summary_large_image",
+    card: 'summary_large_image',
     title:
-      "BrainDAO - Building a more intelligent future through the IQ token.",
+      'BrainDAO - Building a more intelligent future through the IQ token.',
     description:
-      "Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.",
-    images: ["https://braindao.org/images/og-image.png"],
-    site: "@Everipedia",
-    creator: "@Everipedia",
-  },
+      'Join our mission to expand the scope of human knowledge with BrainDAO, the governing DAO powering the IQ ecosystem.',
+    images: ['https://braindao.org/images/og-image.png'],
+    site: '@Everipedia',
+    creator: '@Everipedia'
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={montserrat.className}>
+    <html lang='en' className={montserrat.className}>
       <head>
-        <link rel="canonical" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel='canonical' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin=''
         />
       </head>
       <body>
         <ClientProviders>
-          <div className="overflow-x-hidden">
+          <div className='overflow-x-hidden'>
             {children}
             <Footer />
           </div>
---
+++ src/components/layouts/ClientProviders.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { publicClient } from '@/config/wagmi';
+import { ThemeProvider } from 'next-themes';
+import type { PropsWithChildren } from 'react';
+import { WagmiConfig, createConfig } from 'wagmi';
+import { CSPostHogProvider } from './CgPosthogProvider';
+
+const client = createConfig({
+  autoConnect: true,
+  publicClient
+});
+
+const ClientProviders = ({ children }: PropsWithChildren) => {
+  return (
+    <ThemeProvider attribute='class' defaultTheme='dark'>
+      <WagmiConfig config={client}>
+        <CSPostHogProvider>{children}</CSPostHogProvider>
+      </WagmiConfig>
+    </ThemeProvider>
+  );
+};
+
+export default ClientProviders;
