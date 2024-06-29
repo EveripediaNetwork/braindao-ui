4c56898d4ef01c05a38585bee8b297f829e11bde
Adebesin Cell <adebesintolulope80@gmail.com>
2024-06-29 02:02:06 +0100

    fixes pnpm only

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,14 +1,13 @@
-import { configureChains } from "@wagmi/core";
-import { alchemyProvider } from "@wagmi/core/providers/alchemy";
-import { publicProvider } from "@wagmi/core/providers/public";
-import { mainnet } from "wagmi";
+import { configureChains, mainnet } from "wagmi";
 import { goerli } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 import config from "./index";
 
 const chainArray = config.alchemyChain === "goerli" ? goerli : mainnet;
 
 export const { chains, publicClient } = configureChains(
-  [chainArray],
-  [alchemyProvider({ apiKey: config.alchemyApiKey }), publicProvider()]
+	[chainArray],
+	[alchemyProvider({ apiKey: config.alchemyApiKey }), publicProvider()],
 );
