fbc68efc7411a051beb1be45caead6e8af245638
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-03-06 12:30:35 +0100

    lint formatting

--- src/data/Nav.ts
+++ src/data/Nav.ts
@@ -1,32 +1,32 @@
 export const navLinks = [
-	{
-		title: "IQ.wiki",
-		href: "#iq-wiki",
-		target: "",
-	},
-	{
-		title: "AIDEN",
-		href: "#aiden",
-		target: "",
-	},
-	{
-		title: "IQ Treasury",
-		href: "https://iq.iqai.com/dashboard/treasury",
-		target: "_blank",
-	},
-	{
-		title: "IQ AI",
-		href: "https://iqai.io",
-		target: "_blank",
-	},
-	{
-		title: "Learn",
-		href: "https://learn.everipedia.org/iq",
-		target: "_blank",
-	},
-	{
-		title: "Blog",
-		href: "https://iq.wiki/blog",
-		target: "_blank",
-	},
+  {
+    title: "IQ.wiki",
+    href: "#iq-wiki",
+    target: "",
+  },
+  {
+    title: "AIDEN",
+    href: "#aiden",
+    target: "",
+  },
+  {
+    title: "IQ Treasury",
+    href: "https://iq.iqai.com/dashboard/treasury",
+    target: "_blank",
+  },
+  {
+    title: "IQ AI",
+    href: "https://iqai.io",
+    target: "_blank",
+  },
+  {
+    title: "Learn",
+    href: "https://learn.everipedia.org/iq",
+    target: "_blank",
+  },
+  {
+    title: "Blog",
+    href: "https://iq.wiki/blog",
+    target: "_blank",
+  },
 ];
