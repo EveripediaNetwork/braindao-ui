0336ff4f4571fbc094b139032f49dc616ec4a6b7
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-03-06 12:26:33 +0100

    rename IQGPT to AIDEN

--- src/data/Nav.ts
+++ src/data/Nav.ts
@@ -1,32 +1,32 @@
 export const navLinks = [
-  {
-    title: "IQ.wiki",
-    href: "#iq-wiki",
-    target: "",
-  },
-  {
-    title: "IQ GPT",
-    href: "#iq-gpt",
-    target: "",
-  },
-  {
-    title: "IQ Treasury",
-    href: "https://iq.iqai.com/dashboard/treasury",
-    target: "_blank",
-  },
-  {
-    title: "IQ AI",
-    href: "https://iqai.io",
-    target: "_blank",
-  },
-  {
-    title: "Learn",
-    href: "https://learn.everipedia.org/iq",
-    target: "_blank",
-  },
-  {
-    title: "Blog",
-    href: "https://iq.wiki/blog",
-    target: "_blank",
-  },
+	{
+		title: "IQ.wiki",
+		href: "#iq-wiki",
+		target: "",
+	},
+	{
+		title: "AIDEN",
+		href: "#iq-gpt",
+		target: "",
+	},
+	{
+		title: "IQ Treasury",
+		href: "https://iq.iqai.com/dashboard/treasury",
+		target: "_blank",
+	},
+	{
+		title: "IQ AI",
+		href: "https://iqai.io",
+		target: "_blank",
+	},
+	{
+		title: "Learn",
+		href: "https://learn.everipedia.org/iq",
+		target: "_blank",
+	},
+	{
+		title: "Blog",
+		href: "https://iq.wiki/blog",
+		target: "_blank",
+	},
 ];
