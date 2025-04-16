39965fe36328e82e0c9a54ee6ec30998920435aa
Solexi <ezehsophia4321@gmail.com>
2025-04-16 06:06:19 +0100

    Refactor theme handling in multiple components to use useThemeImage hook

--- src/components/layouts/ecosystem/Iqwiki.tsx
+++ src/components/layouts/ecosystem/Iqwiki.tsx
@@ -1,101 +1,93 @@
 "use client";
 
 import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
-import { useTheme } from "next-themes";
+import useThemeImage from "@/modules/helpers/useThemeImage";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { TokenBrief } from "../Iqtoken";
 
 const Iqwiki = () => {
-  const { theme } = useTheme();
-  const [appTheme, setAppTheme] = useState("");
+	const imageUrl = useThemeImage(
+		"/images/iqwiki-widget-new.webp",
+		"/images/iqwiki-widget-new-dark.webp",
+	);
+	const imageUrl2 = useThemeImage(
+		"/images/wiki-rank.webp",
+		"/images/wiki-rank-dark.webp",
+	);
+	const imageUrl3 = useThemeImage(
+		"/images/wiki-homepage.webp",
+		"/images/wiki-homepage-dark.webp",
+	);
 
-  useEffect(() => {
-    setAppTheme(theme ?? "");
-  }, [theme]);
-  let imageUrl: string | undefined = undefined;
-
-  let imageUrl2: string | undefined = undefined;
-  let imageUrl3: string | undefined = undefined;
-  if (appTheme === "light") {
-    imageUrl = "/images/iqwiki-widget-new.webp";
-    imageUrl2 = "/images/wiki-rank.webp";
-    imageUrl3 = "/images/wiki-homepage.webp";
-  }
-  if (appTheme === "dark") {
-    imageUrl = "/images/iqwiki-widget-new-dark.webp";
-    imageUrl2 = "/images/wiki-rank-dark.webp";
-    imageUrl3 = "/images/wiki-homepage-dark.webp";
-  }
-
-  return (
-    <div id="iq-wiki">
-      <TokenBrief
-        title="IQ.wiki"
-        description="IQ.wiki, the world's largest blockchain and cryptocurrency encyclopedia, is a Polygon-based platform, featuring thousands of pages, that uses IPFS for data storage and on-chain edits. IQ token stakers govern the platform, which offers a gasless experience for contributors and utilizes OpenAI's GPT-3 for article summarization."
-        action="https://iq.wiki"
-        buttonText="Explore IQ.wiki"
-      />
-      <div className="border overflow-hidden rounded-xl mt-16 dark:border-whiteAlpha-400 border-gray200 dark:bg-gray700 bg-white grid md:grid-cols-3">
-        <div className="col-span-1 pb-3 sm:pb-0 pl-3 pt-4 lg:pl-6 lg:pt-8">
-          <p className="max-w-[390px] text-sm xl:text-xl dark:text-whiteAlpha-800 text-gray600">
-            Explore a diverse range of wikis designed to guide you through the
-            realms of{" "}
-            <span className="font-semibold">
-              cryptocurrency and blockchain.
-            </span>
-          </p>
-        </div>
-        <div className="col-span-2">
-          <div className="h-[220px] min-[500px]:h-[280px] sm:h-[400px] md:h-[280px] lg:h-[400px] 2xl:h-[450px] w-full relative">
-            {imageUrl3 && (
-              <Image
-                src={imageUrl3}
-                alt=""
-                fill
-                sizes="(max-width:500px) 100vw, 50vw"
-              />
-            )}
-          </div>
-        </div>
-      </div>
-      <InViewAnimateBottom>
-        <div className="flex flex-col md:flex-row gap-2 min-[500px]:gap-6 sm:gap-[18px] mt-2 min-[500px]:mt-6 sm:mt-[18px]">
-          <div className="md:flex-1 rounded-xl relative overflow-hidden dark:bg-gray700 bg-white h-[300px] min-[500px]:h-[400px] sm:h-[550px] md:h-[300px] lg:h-[470px] xl:h-[440px] 2xl:h-[528px]">
-            {imageUrl && (
-              <Image
-                src={imageUrl}
-                alt=""
-                fill
-                sizes="(max-width:480px) 100vw, 50vw"
-              />
-            )}
-          </div>
-          <div className="md:flex-1 flex flex-col gap-2 min-[500px]:gap-6 sm:gap-[18px] justify-between">
-            <div className="w-full relative h-[280px] min-[500px]:h-[350px] sm:h-[450px] md:h-full rounded-xl dark:bg-gray700 bg-white">
-              {imageUrl2 && (
-                <Image
-                  src={imageUrl2}
-                  alt=""
-                  fill
-                  sizes="(max-width:480px) 100vw, 50vw"
-                />
-              )}
-            </div>
-            <div className="p-3 lg:p-5 flex items-center dark:bg-gray700 bg-white border dark:border-whiteAlpha-400 border-gray200 justify-between gap-2 rounded-xl">
-              <span className="font-medium md:text-sm lg:text-lg xl:text-xl dark:text-whiteAlpha-800 text-gray600">
-                Integrations:
-              </span>
-              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-eth bg-contain bg-no-repeat" />
-              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-graph bg-contain bg-no-repeat" />
-              <div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-coingecko bg-contain bg-no-repeat" />
-              <div className="2xl:w-32 2xl:h-12 md:w-20 md:h-8 w-24 h-10 lg:w-28 lg:h-10 dark:bg-magic bg-magic-white bg-contain bg-no-repeat" />
-            </div>
-          </div>
-        </div>
-      </InViewAnimateBottom>
-    </div>
-  );
+	return (
+		<div id="iq-wiki">
+			<TokenBrief
+				title="IQ.wiki"
+				description="IQ.wiki, the world's largest blockchain and cryptocurrency encyclopedia, is a Polygon-based platform, featuring thousands of pages, that uses IPFS for data storage and on-chain edits. IQ token stakers govern the platform, which offers a gasless experience for contributors and utilizes OpenAI's GPT-3 for article summarization."
+				action="https://iq.wiki"
+				buttonText="Explore IQ.wiki"
+			/>
+			<div className="border overflow-hidden rounded-xl mt-16 dark:border-whiteAlpha-400 border-gray200 dark:bg-gray700 bg-white grid md:grid-cols-3">
+				<div className="col-span-1 pb-3 sm:pb-0 pl-3 pt-4 lg:pl-6 lg:pt-8">
+					<p className="max-w-[390px] text-sm xl:text-xl dark:text-whiteAlpha-800 text-gray600">
+						Explore a diverse range of wikis designed to guide you through the
+						realms of{" "}
+						<span className="font-semibold">
+							cryptocurrency and blockchain.
+						</span>
+					</p>
+				</div>
+				<div className="col-span-2">
+					<div className="h-[220px] min-[500px]:h-[280px] sm:h-[400px] md:h-[280px] lg:h-[400px] 2xl:h-[450px] w-full relative">
+						{imageUrl3 && (
+							<Image
+								src={imageUrl3}
+								alt=""
+								fill
+								sizes="(max-width:500px) 100vw, 50vw"
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+			<InViewAnimateBottom>
+				<div className="flex flex-col md:flex-row gap-2 min-[500px]:gap-6 sm:gap-[18px] mt-2 min-[500px]:mt-6 sm:mt-[18px]">
+					<div className="md:flex-1 rounded-xl relative overflow-hidden dark:bg-gray700 bg-white h-[300px] min-[500px]:h-[400px] sm:h-[550px] md:h-[300px] lg:h-[470px] xl:h-[440px] 2xl:h-[528px]">
+						{imageUrl && (
+							<Image
+								src={imageUrl}
+								alt=""
+								fill
+								sizes="(max-width:480px) 100vw, 50vw"
+							/>
+						)}
+					</div>
+					<div className="md:flex-1 flex flex-col gap-2 min-[500px]:gap-6 sm:gap-[18px] justify-between">
+						<div className="w-full relative h-[280px] min-[500px]:h-[350px] sm:h-[450px] md:h-full rounded-xl dark:bg-gray700 bg-white">
+							{imageUrl2 && (
+								<Image
+									src={imageUrl2}
+									alt=""
+									fill
+									sizes="(max-width:480px) 100vw, 50vw"
+								/>
+							)}
+						</div>
+						<div className="p-3 lg:p-5 flex items-center dark:bg-gray700 bg-white border dark:border-whiteAlpha-400 border-gray200 justify-between gap-2 rounded-xl">
+							<span className="font-medium md:text-sm lg:text-lg xl:text-xl dark:text-whiteAlpha-800 text-gray600">
+								Integrations:
+							</span>
+							<div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-eth bg-contain bg-no-repeat" />
+							<div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-graph bg-contain bg-no-repeat" />
+							<div className="w-8 sm:w-10 h-8 sm:h-10 md:w-8 md:h-8 lg:w-10 lg:h-10 2xl:w-12 2xl:h-12 bg-coingecko bg-contain bg-no-repeat" />
+							<div className="2xl:w-32 2xl:h-12 md:w-20 md:h-8 w-24 h-10 lg:w-28 lg:h-10 dark:bg-magic bg-magic-white bg-contain bg-no-repeat" />
+						</div>
+					</div>
+				</div>
+			</InViewAnimateBottom>
+		</div>
+	);
 };
 
 export default Iqwiki;
--- src/components/layouts/ecosystem/iqsocial.tsx
+++ src/components/layouts/ecosystem/iqsocial.tsx
@@ -1,118 +1,114 @@
 "use client";
-
 import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
-import { useTheme } from "next-themes";
+import useThemeImage from "@/modules/helpers/useThemeImage";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { TokenBrief } from "../Iqtoken";
 
-export const getImageUrl = (
-  appTheme: string,
-  imageName: string,
-  num?: number
-) => {
-  let imageUrl = `/images/${imageName}-${
-    appTheme === "dark" ? "dark" : "light"
-  }`;
-  if (num !== undefined) {
-    imageUrl += `-${num}`;
-  }
-  imageUrl += ".webp";
-  return imageUrl;
-};
-
 export const FeatureList = ({
-  title,
-  description,
-  icon,
-  delay,
+	title,
+	description,
+	icon,
+	delay,
 }: {
-  title: string;
-  description: string;
-  icon: JSX.Element;
-  delay?: number;
+	title: string;
+	description: string;
+	icon: JSX.Element;
+	delay?: number;
 }) => {
-  return (
-    <InViewAnimateBottom className="flex flex-1 flex-col gap-4 items-start">
-      <div className="w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100">
-        {icon}
-      </div>
-      <div className="">
-        <h4 className="dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1">
-          {title}
-        </h4>
-        <p className="text-sm lg:text-base">{description}</p>
-      </div>
-    </InViewAnimateBottom>
-  );
+	return (
+		<InViewAnimateBottom className="flex flex-1 flex-col gap-4 items-start">
+			<div className="w-12 h-12 rounded-full flex justify-center text-xl dark:text-brand-800 text-brand-500 items-center border-8 dark:border-brand-200 border-brand-50 dark:bg-brand-400 bg-brand-100">
+				{icon}
+			</div>
+			<div className="">
+				<h4 className="dark:text-whiteAlpha-900 text-gray800 font-semibold text-lg md:text-xl mb-1">
+					{title}
+				</h4>
+				<p className="text-sm lg:text-base">{description}</p>
+			</div>
+		</InViewAnimateBottom>
+	);
 };
 
 const Iqsocial = () => {
-  const { theme } = useTheme();
-  const [appTheme, setAppTheme] = useState("");
+	const imageUrl = useThemeImage(
+		"/images/iq-social-light-1.webp",
+		"/images/iq-social-dark-1.webp",
+	);
+	const imageUrl2 = useThemeImage(
+		"/images/iq-social-light-2.webp",
+		"/images/iq-social-dark-2.webp",
+	);
+	const imageUrl3 = useThemeImage(
+		"/images/iq-social-light-3.webp",
+		"/images/iq-social-dark-3.webp",
+	);
 
-  useEffect(() => {
-    setAppTheme(theme ?? "");
-  }, [theme]);
+	return (
+		<div
+			id="iq-social"
+			className="max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative"
+		>
+			<TokenBrief
+				title="IQ social"
+				description={`IQ.social is an AI-powered social forum designed to enhance users' knowledge and awareness of the cryptocurrency and blockchain industry. The platform is governed by HiIQ stakers who curate and rank all of the articles submitted to IQ.social.`}
+				action="https://iq.social"
+				buttonText="Explore IQ Social"
+			/>
 
-  return (
-    <div
-      id="iq-social"
-      className="max-w-[1536px] px-4 sm:px-10 xl:px-[120px] mx-auto flex flex-col gap-10 xl:gap-16 pt-[48px] sm:pt-[80px] 2xl:pt-24 relative"
-    >
-      <TokenBrief
-        title="IQ social"
-        description={`IQ.social is an AI-powered social forum designed to enhance users' knowledge and awareness of the cryptocurrency and blockchain industry. The platform is governed by HiIQ stakers who curate and rank all of the articles submitted to IQ.social.`}
-        action="https://iq.social"
-        buttonText="Explore IQ Social"
-      />
-
-      <InViewAnimateBottom>
-        <div className="grid grid-cols-1 md:grid-cols-[1.5fr_1fr] gap-5">
-          <Image
-            src={getImageUrl(appTheme, "iq-social", 1)}
-            alt="IQ.social Illustration"
-            width={750}
-            height={490}
-          />
-          <div>
-            <div className="border border-gray-200 dark:border-white/40 pb-6 bg-white rounded-3xl dark:bg-gray-700">
-              <Image
-                src={getImageUrl(appTheme, "iq-social", 2)}
-                alt="IQ.social Illustration"
-                width={590}
-                height={340}
-                className="rounded-t-3xl"
-              />
-              <p className="text-gray-600 dark:text-white/70 pl-10 font-semibold text-xs xl:text-lg pr-5 md:pr-0">
-                Share your curated crypto news with the community by submitting
-                links under your preferred categories.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="border border-gray-200 dark:border-white/40 mt-2.5 bg-white rounded-3xl dark:bg-gray-700 px-10 grid grid-cols-1 md:grid-cols-[1.5fr_1fr] gap-5">
-          <Image
-            src={getImageUrl(appTheme, "iq-social", 3)}
-            alt="IQ.social Illustration"
-            width={560}
-            height={200}
-            className="h-full w-full object-cover mt-5 md:mt-0"
-          />
-          <div className="pt-0 pb-10 md:py-10">
-            <h1 className="text-gray-600 dark:text-white/70 font-medium text-xs xl:text-base">
-              Categories
-            </h1>
-            <p className="text-gray-600 dark:text-white/70 font-semibold text-xs xl:text-lg">
-              Discover a diverse array of categories on IQ social, including the
-              latest in AI news, nfts, development, cryptocurrencies and
-              regulations.
-            </p>
-          </div>
-        </div>
-      </InViewAnimateBottom>
-    </div>
-  );
+			<InViewAnimateBottom>
+				<div className="grid grid-cols-1 md:grid-cols-[1.5fr_1fr] gap-5">
+					{imageUrl && (
+						<Image
+							src={imageUrl}
+							alt="IQ.social Illustration"
+							width={750}
+							height={490}
+						/>
+					)}
+					<div>
+						<div className="border border-gray-200 dark:border-white/40 pb-6 bg-white rounded-3xl dark:bg-gray-700">
+							{imageUrl2 && (
+								<Image
+									src={imageUrl2}
+									alt="IQ.social Illustration"
+									width={590}
+									height={340}
+									className="rounded-t-3xl"
+								/>
+							)}
+							<p className="text-gray-600 dark:text-white/70 pl-10 font-semibold text-xs xl:text-lg pr-5 md:pr-0">
+								Share your curated crypto news with the community by submitting
+								links under your preferred categories.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="border border-gray-200 dark:border-white/40 mt-2.5 bg-white rounded-3xl dark:bg-gray-700 px-10 grid grid-cols-1 md:grid-cols-[1.5fr_1fr] gap-5">
+					{imageUrl3 && (
+						<Image
+							src={imageUrl3}
+							alt="IQ.social Illustration"
+							width={590}
+							height={340}
+							className="rounded-t-3xl"
+						/>
+					)}
+					<div className="pt-0 pb-10 md:py-10">
+						<h1 className="text-gray-600 dark:text-white/70 font-medium text-xs xl:text-base">
+							Categories
+						</h1>
+						<p className="text-gray-600 dark:text-white/70 font-semibold text-xs xl:text-lg">
+							Discover a diverse array of categories on IQ social, including the
+							latest in AI news, nfts, development, cryptocurrencies and
+							regulations.
+						</p>
+					</div>
+				</div>
+			</InViewAnimateBottom>
+		</div>
+	);
 };
 
 export default Iqsocial;
