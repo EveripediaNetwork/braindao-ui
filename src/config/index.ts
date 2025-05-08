e8f82f26fdcd86f0b6a79c4abfdb720af9a86558
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-05-08 15:22:19 +0100

    feat: add IQ gateway URL and key to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,20 @@
 const config = {
-  iqAddress:
-    process.env.NEXT_PUBLIC_IQ_ADDRESS ||
-    "0x0552D756a3E92Aa874EF60F61b7a29030373e869",
-  hiiqAddress:
-    process.env.NEXT_PUBLIC_HIIQ_ADDRESS ||
-    "0xC03bCACC5377b7cc6634537650A7a1D14711c1A3",
-  alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
-  alchemyChain: process.env.NEXT_PUBLIC_ALCHEMY_CHAIN || "goerli",
-  chainId: process.env.NEXT_PUBLIC_CHAIN_ID || "5",
-  cmcApiKey: process.env.NEXT_PUBLIC_CMC_API_KEY || "",
-  walletConnectProjectId:
-    process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ||
-    "e6567b95-7acb-4954-8673-76d34981e18f",
-  posthogApiKey: process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
+	iqAddress:
+		process.env.NEXT_PUBLIC_IQ_ADDRESS ||
+		"0x0552D756a3E92Aa874EF60F61b7a29030373e869",
+	hiiqAddress:
+		process.env.NEXT_PUBLIC_HIIQ_ADDRESS ||
+		"0xC03bCACC5377b7cc6634537650A7a1D14711c1A3",
+	alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
+	alchemyChain: process.env.NEXT_PUBLIC_ALCHEMY_CHAIN || "goerli",
+	chainId: process.env.NEXT_PUBLIC_CHAIN_ID || "5",
+	cmcApiKey: process.env.NEXT_PUBLIC_CMC_API_KEY || "",
+	walletConnectProjectId:
+		process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ||
+		"e6567b95-7acb-4954-8673-76d34981e18f",
+	posthogApiKey: process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
+	iqGatewayUrl: process.env.NEXT_PUBLIC_IQ_GATEWAY_URL || "",
+	iqGatewayKey: process.env.NEXT_PUBLIC_IQ_GATEWAY_KEY || "",
 };
 
 export default config;
