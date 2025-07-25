f1782987edb2491c90695a459dce57017716894a
Solexi <ezehsophia4321@gmail.com>
2025-07-25 08:08:43 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/layouts/braindao-team/index.tsx
+++ src/components/layouts/braindao-team/index.tsx
@@ -19,65 +19,55 @@ const TeamCard = ({
 	wikiLink,
 }: TTeam) => {
 	return (
-		<InViewAnimateBottom className="mx-auto w-full sm:max-w-[351px] md:max-w-[328px]">
-			<div className="border dark:bg-gray700 bg-white dark:border-whiteAlpha-400 border-gray200 rounded-xl px-4 py-5">
-				<div className="rounded-md relative h-[410px] sm:h-[373px] md:h-[318px] w-full lg:h-[300px] xl:h-[300px] 2xl:h-[324px] overflow-hidden hover:scale-105 ease-in-out duration-300">
+		<InViewAnimateBottom className="w-full">
+			<div className="flex flex-col">
+				<div className="relative w-full aspect-square rounded-tl-xl rounded-tr-xl mb-4 overflow-hidden bg-black shadow-[0_-1.12px_0px_-0px_#FF5CAA]">
 					<Image
 						src={src || ""}
 						alt={`${name}'s profile picture`}
 						fill
-						sizes="50vw"
-						className="object-cover object-top"
+						sizes="(max-width: 640px) 50vw, (max-width: 1024px) 30vw, 20vw"
+						className="object-cover object-top rounded-tl-xl rounded-tr-xl"
 					/>
-					<div className="absolute top-0 left-0 h-full w-full bg-transparent px-5 pb-5 flex flex-col justify-end">
-						<div className="dark:bg-gray700 bg-white relative rounded-[4px] py-2 px-1 flex flex-col gap-2 items-center w-full">
-							<div className="rounded-full h-4 w-4 dark:bg-brand-800 bg-brand-500 absolute -top-2" />
-							<div className="flex flex-col items-center">
-								<h4 className="text-foreground text-sm font-semibold">
-									{name || ""}
-								</h4>
-								<p className="dark:text-brand-800 text-brand-500 text-center font-medium text-xs">
-									{role || ""}
-								</p>
-							</div>
-							<div className="flex justify-center">
-								<div className="flex gap-2 items-center">
-									{twitterLink && (
-										<a
-											href={twitterLink}
-											target="_blank"
-											className="w-5 md:w-4 h-5 md:h-4 bg-no-repeat bg-contain dark:bg-twitter-dark bg-twitter"
-											rel="noreferrer"
-											aria-label={`${name}'s Twitter profile`}
-										>
-											<span className="sr-only">Twitter</span>
-										</a>
-									)}
-									{wikiLink && (
-										<a
-											href={wikiLink}
-											target="_blank"
-											className="w-6 md:w-5 h-4 md:h-4 bg-no-repeat bg-contain dark:bg-everipedia-dark bg-everipedia"
-											rel="noreferrer"
-											aria-label={`${name}'s Wiki page`}
-										>
-											<span className="sr-only">Wiki</span>
-										</a>
-									)}
-									{linkedlnLink && (
-										<a
-											href={linkedlnLink}
-											target="_blank"
-											className="w-5 md:w-4 h-5 md:h-4 bg-no-repeat bg-contain dark:bg-linkedln-dark bg-linkedln"
-											rel="noreferrer"
-											aria-label={`${name}'s LinkedIn profile`}
-										>
-											<span className="sr-only">LinkedIn</span>
-										</a>
-									)}
-								</div>
-							</div>
-						</div>
+				</div>
+
+				<div className="flex flex-col items-start font-mo">
+					<p className="text-primary text-xs font-medium mb-2">{role || ""}</p>
+					<h4 className="text-white text-s font-semibold mb-3">{name || ""}</h4>
+					<div className="flex gap-3 items-center bg-neutral-900 border border-neutral-700 rounded-2xl py-2 px-3.5">
+						{twitterLink && (
+							<a
+								href={twitterLink}
+								target="_blank"
+								className="w-5 h-5 bg-no-repeat bg-contain dark:bg-twitter-dark bg-twitter opacity-70 hover:opacity-100 transition-opacity"
+								rel="noreferrer"
+								aria-label={`${name}'s Twitter profile`}
+							>
+								<span className="sr-only">Twitter</span>
+							</a>
+						)}
+						{wikiLink && (
+							<a
+								href={wikiLink}
+								target="_blank"
+								className="w-7 h-5 bg-no-repeat bg-contain bg-everipedia opacity-70 hover:opacity-100 transition-opacity"
+								rel="noreferrer"
+								aria-label={`${name}'s Wiki page`}
+							>
+								<span className="sr-only">Wiki</span>
+							</a>
+						)}
+						{linkedlnLink && (
+							<a
+								href={linkedlnLink}
+								target="_blank"
+								className="w-5 h-5 bg-no-repeat bg-contain bg-linkedln opacity-70 hover:opacity-100 transition-opacity"
+								rel="noreferrer"
+								aria-label={`${name}'s LinkedIn profile`}
+							>
+								<span className="sr-only">LinkedIn</span>
+							</a>
+						)}
 					</div>
 				</div>
 			</div>
@@ -86,28 +76,40 @@ const TeamCard = ({
 };
 const BraindaoTeam = () => {
 	return (
-		<div className="dark:bg-gray800 bg-[#F9FAFB] text-gray600">
-			<div className="max-w-[1536px] px-4 md:px-20 xl:px-24 2xl:px-[120px] mx-auto py-[48px] sm:py-[80px] 2xl:py-24">
-				<h2 className="text-center text-3xl sm:text-4xl xl:text-5xl  font-semibold mb-10 xl:mb-14">
-					Humans of BrainDAO
-				</h2>
-				<div className="flex flex-col gap-5 sm:gap-16 mt-10 md:mt-14">
-					<div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-5 sm:gap-y-10 2xl:gap-y-16 gap-x-8 lg:gap-x-4">
-						{braindaoTeam.map((team, idx) => {
-							return (
-								<TeamCard
-									src={team.src}
-									name={team.name}
-									role={team.role}
-									key={team.name}
-									linkedlnLink={team.linkedlnLink}
-									twitterLink={team.twitterLink}
-									wikiLink={team.wikiLink}
-								/>
-							);
-						})}
-					</div>
+		<div className="px-4 md:px-10 xl:container xl:mx-auto xl:px-4 py-[48px] sm:py-[80px] 2xl:py-24">
+			<div className="flex items-start flex-col lg:flex-row w-full justify-between">
+				<div className="xl:w-[400px] xl:flex-shrink-0">
+					<InViewAnimateBottom>
+						<h3 className="font-semibold text-2xl sm:text-3xl xl:text-4xl font-satoshi text-primary">
+							Meet the Team
+						</h3>
+					</InViewAnimateBottom>
 				</div>
+				<div className="mt-2 xl:mt-0">
+					<InViewAnimateBottom>
+						<p className="flex-1 lg:text-lg text-muted-foreground xl: max-w-[650px] 2xl:max-w-[700px]">
+							BrainDAO is made up of a diverse team united by our mission,
+							hailing from various corners of the globe.
+						</p>
+					</InViewAnimateBottom>
+				</div>
+			</div>
+			<div className="relative grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-10 md:mt-14 border-t border-b border-neutral-700">
+				{braindaoTeam.map((team) => (
+					<div
+						key={team.name}
+						className="p-4 sm:p-6 lg:p-4 border-neutral-700 border-b sm:[&:nth-child(odd)]:border-r sm:[&:nth-child(2n)]:border-r-0 lg:[&:nth-child(2n)]:border-r lg:[&:nth-child(3n)]:border-r-0 xl:[&:nth-child(3n)]:border-r xl:[&:nth-child(4n)]:border-r-0"
+					>
+						<TeamCard
+							src={team.src}
+							name={team.name}
+							role={team.role}
+							linkedlnLink={team.linkedlnLink}
+							twitterLink={team.twitterLink}
+							wikiLink={team.wikiLink}
+						/>
+					</div>
+				))}
 			</div>
 		</div>
 	);
