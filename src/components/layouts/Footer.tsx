80d794d8a934d7efc98fde6b3b5b41208e528d06
Solexi <ezehsophia4321@gmail.com>
2025-07-29 17:46:28 +0100

    Refactor Footer component layout and styles

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
 		<footer className="bg-neutral-950">
 			<div className="xl:container xl:mx-auto px-4 md:px-10 xl:px-4 flex flex-col gap-6 py-12">
 				<div className="flex flex-col lg:flex-row gap-6 w-full">
-					<div className="flex flex-col sm:flex-row w-full  gap-6 md:gap-40 lg:gap-24">
+					<div className="flex flex-row w-full gap-6 md:gap-40 lg:gap-24 items-center">
 						<div className="text-muted-foreground">
 							<h2 className="text-sm font-bold mb-3 font-satoshi">
 								Subscribe to our newsletter
@@ -34,12 +34,12 @@ const Footer = () => {
 						</div>
 						<button
 							type="button"
-							className="bg-primary hover:bg-primary/50 text-white px-8 py-1.5 rounded-full text-sm font-medium transition-colors whitespace-nowrap self-center mb-10"
+							className="bg-primary hover:bg-primary/50 text-white px-4 sm:px-8 py-1.5 rounded-full text-xs sm:text-sm font-medium transition-colors whitespace-nowrap self-center lg:mb-10"
 						>
 							Subscribe
 						</button>
 					</div>
-					<div className="flex flex-col sm:flex-row w-full justify-between text-sm gap-8 lg:gap-0">
+					<div className="flex flex-row w-full justify-between text-sm gap-8 lg:gap-0">
 						<div>
 							<h3 className="font-bold mb-4 text-muted-foreground font-satoshi text-sm">
 								For business Inquiries
@@ -55,7 +55,7 @@ const Footer = () => {
 							<h3 className="font-bold mb-4 text-muted-foreground font-satoshi">
 								Follow us
 							</h3>
-							<div className="space-y-2 flex flex-col sm:text-right text-foreground hover:text-white transition-colors">
+							<div className="space-y-2 flex flex-col text-right text-foreground hover:text-white transition-colors">
 								{socialLinks.map((link) => (
 									<a
 										key={link.name}
@@ -74,33 +74,31 @@ const Footer = () => {
 						<h2 className="text-sm font-bold mb-0 font-satoshi text-white">
 							Company
 						</h2>
-						<div className="flex flex-row items-center gap-x-3 overflow-x-auto md:overflow-visible md:flex-wrap scrollbar-hide xl:max-w-[400px]">
+						<div className="flex flex-row items-center gap-x-6 sm:gap-x-4 md:gap-x-6 lg:gap-x-5 gap-y-4 flex-wrap xl:max-w-[420px]">
 							{products.map((link, linkIndex) => (
 								<React.Fragment key={link.name}>
 									<a
 										href={link.href}
-										className="block text-white hover:text-gray-300 transition-colors text-xl lg:text-2xl font-light whitespace-nowrap flex-shrink-0 relative overflow-hidden group"
+										className="block text-white hover:text-gray-300 transition-colors text-sm sm:text-lg lg:text-2xl font-light whitespace-nowrap flex-shrink-0 relative overflow-hidden group"
 									>
 										<span className="relative z-10">{link.name}</span>
 										<div className="absolute bottom-0 left-0 w-full h-0 bg-[#FF5CAA66] transition-all duration-300 ease-out group-hover:h-[50%]" />
 									</a>
 									{linkIndex < products.length - 1 && (
-										<span className="text-neutral-700 text-2xl lg:text-3xl font-light flex-shrink-0">
-											/
-										</span>
+										<div className="h-8 border-[0.5px] border-neutral-700 rotate-45 transform" />
 									)}
 								</React.Fragment>
 							))}
 						</div>
 					</div>
-					<div className="flex flex-col lg:flex-row items-start lg:items-center text-muted-foreground gap-6 lg:gap-60 w-full">
-						<span className="flex-1">
+					<div className="flex flex-row items-start sm:items-center text-muted-foreground gap-20 sm:gap-60 w-full">
+						<span className="flex-1 text-xs sm:text-sm">
 							BrainDAO is creating an intelligent future as the governing DAO
 							behind the IQ ecosystem, supported by Brainfund.
 						</span>
 						<button
 							type="button"
-							className="text-primary self-start lg:self-center"
+							className="text-primary self-start sm:self-center"
 							onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
 							title="Scroll to top"
 							aria-label="Scroll to top"
@@ -111,15 +109,17 @@ const Footer = () => {
 				</div>
 			</div>
 			<div className="bg-neutral-900">
-				<div className="flex flex-col md:flex-row items-center justify-between py-4 xl:container xl:mx-auto text-sm text-[#F3F4F6] px-4 md:px-10 xl:px-4">
+				<div className="flex flex-col md:flex-row items-center gap-5 md:gap-0 justify-between py-4 xl:container xl:mx-auto text-sm text-[#F3F4F6] px-4 md:px-10 xl:px-4 w-full">
 					<span>© 2025 — Powered by IQ and BrainDAO.</span>
-					<a
-						href="https://iq.wiki/privacy"
-						className="hover:text-white transition-colors"
-					>
-						Privacy & Cookies policy
-					</a>
-					<span>All rights reserved</span>
+					<div className="flex flex-row items-center justify-between sm:w-[50%] gap-9 sm:gap-0">
+						<a
+							href="https://iq.wiki/privacy"
+							className="hover:text-white transition-colors"
+						>
+							Privacy & Cookies policy
+						</a>
+						<span>All rights reserved</span>
+					</div>
 				</div>
 			</div>
 		</footer>
