4a8c1f72ea8ed8e101eb594cebfc314d06dd56a3
Damola18 <olutokeezekiel18@gmail.com>
2025-08-04 16:54:18 +0100

    iq wiki section update

--- src/components/layouts/ecosystem/Iqwiki.tsx
+++ src/components/layouts/ecosystem/Iqwiki.tsx
@@ -4,74 +4,95 @@ import { TokenBrief } from "@/app/_components/token-brief";
 import InViewAnimateBottom from "@/components/transitions/InViewAnimateBottom";
 import Image from "next/image";
 import React from "react";
-import IqWikiLanding from "./images/iq-wiki/iqwiki-landing.webp";
+import IqWikiLandingAlt from "./images/iq-wiki/IQ.wiki.svg";
 import WidgetImage from "./images/iq-wiki/widgets.webp";
 import WikiRankImage from "./images/iq-wiki/wiki-rank.webp";
 
 const Iqwiki = () => {
 	return (
 		<div id="iq-wiki" className="bg-black text-muted-foreground">
-			<div className="px-4 md:px-10 xl:container xl:mx-auto xl:px-4 pb-[48px] sm:pb-[80px] xl:pb-24">
+			<div className="px-4 xl:container xl:mx-auto xl:px-4 py-24 sm:py-20 xl:py-0">
 				<TokenBrief
 					title="IQ.wiki"
 					description="IQ.wiki, the world's largest blockchain and cryptocurrency encyclopedia, is a Polygon-based platform, featuring thousands of pages, that uses IPFS for data storage and on-chain edits. IQ token stakers govern the platform, which offers a gasless experience for contributors and utilizes Sophia Agent for article summarization."
 					action="https://iq.wiki"
 					buttonText="Explore IQ.wiki"
 				/>
-			</div>
 
-			<div className="px-4 pb-8 md:px-10 xl:container xl:mx-auto xl:px-4 xl:pb-24">
-				<div
-					className="border overflow-hidden rounded-xl mt-16 border-gray-700 bg-black grid md:grid-cols-3 relative"
-					style={{
-						backgroundImage: "url(/images/shadow.png)",
-						backgroundSize: "cover",
-						backgroundPosition: "center",
-					}}
-				>
-					<div className="col-span-1 pb-3 sm:pb-0 pl-3 pt-4 lg:pl-6 lg:pt-8">
-						<p className="text-sm xl:text-lg font-medium text-muted-foreground">
-							Explore a diverse range of wikis designed to guide you through the
-							realms of cryptocurrency and blockchain.
-						</p>
-					</div>
-					<div className="col-span-2">
-						<div className="h-[220px] min-[500px]:h-[280px] sm:h-[400px] md:h-[280px] lg:h-[400px] 2xl:h-[450px] w-full relative">
-							<Image
-								src={IqWikiLanding}
-								alt="IQ.wiki landing page interface"
-								fill
-								sizes="(max-width:500px) 100vw, 50vw"
-							/>
+				<div className="flex flex-col gap-6">
+					<div
+						className="flex flex-col md:flex-row border overflow-hidden mt-16 border-neutral-700 rounded-3xl bg-black relative"
+						style={{
+							backgroundImage: "url(/images/shadow.png)",
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+						}}
+					>
+						<div className="pl-3 pt-8">
+							<p className="text-sm lg:text-base max-w-xl font-medium text-muted-foreground">
+								Explore a diverse range of wikis designed to guide you through
+								the realms of cryptocurrency and blockchain.
+							</p>
 						</div>
-					</div>
-				</div>
 
-				<InViewAnimateBottom>
-					<div className="flex flex-col md:flex-row gap-2 min-[500px]:gap-6 sm:gap-[18px] mt-2 min-[500px]:mt-6 sm:mt-[18px]">
-						<div className="md:flex-1 rounded-xl relative overflow-hidden h-[300px] min-[500px]:h-[400px] sm:h-[550px] md:h-[300px] lg:h-[470px] xl:h-[440px] 2xl:h-[528px]">
-							<Image
-								src={WidgetImage}
-								alt="IQ.wiki widgets and tools interface"
-								fill
-								sizes="(max-width:480px) 100vw, 50vw"
-							/>
-						</div>
-						<div className="md:flex-1 flex flex-col gap-2 min-[500px]:gap-6 sm:gap-[18px] justify-between">
-							<div className="w-full relative h-[280px] min-[500px]:h-[350px] sm:h-[450px] md:h-full rounded-xl ">
+						<div className="w-full relative pt-8 pl-3 sm:pl-16 xl:pl-8">
+							<div className="relative">
 								<Image
-									src={WikiRankImage}
-									alt="IQ.wiki ranking showing details of cryptocurrencies and other blockchain projects"
-									fill
-									sizes="(max-width:480px) 100vw, 50vw"
+									src={IqWikiLandingAlt}
+									alt="IQ.wiki landing page interface"
+									className="w-full h-full object-cover"
+								/>
+								<div
+									className="absolute inset-0"
+									style={{
+										background:
+											"linear-gradient(359.56deg, #0A0A0A 5.2%, rgba(26, 27, 30, 0.1) 87.64%)",
+									}}
 								/>
 							</div>
 						</div>
 					</div>
-				</InViewAnimateBottom>
+
+					<InViewAnimateBottom>
+						<div className="w-full flex flex-col justify-between items-center md:flex-row gap-6">
+							<div className="w-full md:w-1/2">
+								<Image
+									src={WidgetImage}
+									alt="IQ.wiki widgets and tools interface"
+									className="w-full h-auto object-contain"
+								/>
+							</div>
+
+							<div className="w-full md:w-1/2">
+								<Image
+									src={WikiRankImage}
+									alt="IQ.wiki ranking showing details of cryptocurrencies and other blockchain projects"
+									className="w-full h-auto object-contain"
+								/>
+							</div>
+						</div>
+					</InViewAnimateBottom>
+				</div>
 			</div>
 		</div>
 	);
 };
+<div className="w-full relative pt-8 pl-4 sm:pl-16 xl:pl-8 ">
+	<Image
+		src={IqWikiLandingAlt}
+		alt="IQ.wiki landing page interface"
+		className="w-full h-full object-cover"
+	/>
+	<div
+		className="absolute inset-0"
+		style={{
+			background:
+				"linear-gradient(359.56deg, #0A0A0A 5.2%, rgba(26, 27, 30, 0.1) 87.64%)",
+		}}
+	/>
+</div>;
 
 export default Iqwiki;
+
+// add an extra className to the DetailCard image section so i can add extra css class to reposition of the image
+// dont use DetailCard, use normal css so you can add h-1/2 to the left
