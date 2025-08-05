85483bbf284c9e61424dea015488fd4e3ece09de
Damola18 <olutokeezekiel18@gmail.com>
2025-08-05 07:04:01 +0100

    hero gradient fix

--- src/components/layouts/Hero.tsx
+++ src/components/layouts/Hero.tsx
@@ -56,10 +56,10 @@ const Hero = () => {
 
 	return (
 		<div className="relative overflow-hidden pb-8">
-			<div className="absolute top-0 left-0 right-0 h-80 z-10">
+			<div className="absolute top-0 left-0 right-0 h-[50vh] sm:[50vh] md:h-[90vh] z-10">
 				<img
 					src="/images/gradient-blur-bg.png"
-					alt=""
+					alt="gradient blur"
 					className="w-full h-full object-cover opacity-100"
 				/>
 			</div>
@@ -67,37 +67,39 @@ const Hero = () => {
 			<div className="relative">
 				<div className="max-w-[1536px] mx-auto">
 					<Navbar />
-					<div className="flex flex-col items-center w-full justify-center relative overflow-hidden min-h-[calc(40vh-65px)] md:min-h-[calc(60vh-65px)] lg:min-h-[calc(60vh-65px)] pt-32 md:pt-48 lg:pt-56 pb-8 md:pb-12 lg:pb-16 xl:pb-20">
-						<div className="flex text-center md:text-left items-center flex-col justify-start mx-auto gap-1 sm:gap-5 w-full px-4 sm:px-0 max-w-6xl overflow-hidden">
-							<h1
-								ref={headingRef}
-								className={cn(
-									satoshi.className,
-									"text-2xl sm:text-4xl md:text-5xl lg:text-[50px] xl:text-[60px] 2xl:text-[70px] font-medium text-white !leading-tight text-center overflow-hidden",
-								)}
-							>
-								<span className="block">Building A More</span>
-								<span className="block">Intelligent Future Through</span>
-								<span className="block">
-									the <span className="italic text-primary">IQ Token.</span>
-								</span>
-							</h1>
-							<div className="absolute bottom-4 left-0 right-0 h-1/2 opacity-80">
+					<div className="flex flex-col items-center w-full h-[100vh] justify-center relative overflow-hidden min-h-[calc(40vh-65px)] md:min-h-[calc(60vh-65px)] lg:min-h-[calc(60vh-65px)] pt-0 md:pt-20 lg:pt-2 pb-56 md:pb-40 lg:pb-96 xl:pb-32 -mt-20 md:-mt-32 lg:-mt-0 xl:pt-0">
+						<div className="flex text-center md:text-left items-center flex-col justify-start mx-auto gap-1 sm:gap-5 w-full px-4 sm:px-0 max-w-6xl overflow-hidden ">
+							<div className="absolute bottom-0 left-0 right-0 h-1/2 sm:h-2/3 md:h-2/3 opacity-80 ">
 								<Image
 									src="/images/mesh.png"
 									alt=""
 									width={1200}
-									height={1200}
-									className="w-full object-cover scale-20 pt-12"
+									height={600}
+									className="w-full object-cover"
 								/>
 							</div>
-							<p
-								ref={paragraphRef}
-								className="text-sm sm:text-lg max-w-[513px] lg:max-w-2xl text-center mx-auto lg:text-xl text-white pt-0"
-							>
-								Join our mission to expand the scope of human knowledge with
-								BrainDAO, the governing DAO powering the IQ ecosystem.
-							</p>
+							<div className="w-full flex flex-col gap-4 items-center justify-center">
+								<h1
+									ref={headingRef}
+									className={cn(
+										satoshi.className,
+										"text-2xl sm:text-4xl md:text-5xl lg:text-[50px] xl:text-[60px] 2xl:text-[70px] font-medium text-white !leading-tight text-center overflow-hidden",
+									)}
+								>
+									<span className="block">Building A More</span>
+									<span className="block">Intelligent Future Through</span>
+									<span className="block">
+										the <span className="italic text-primary">IQ Token.</span>
+									</span>
+								</h1>
+								<p
+									ref={paragraphRef}
+									className="text-sm sm:text-lg max-w-[513px] lg:max-w-2xl text-center mx-auto lg:text-xl text-white pt-0 "
+								>
+									Join our mission to expand the scope of human knowledge with
+									BrainDAO, the governing DAO powering the IQ ecosystem.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
