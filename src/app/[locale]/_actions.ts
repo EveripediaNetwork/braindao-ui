7864294b7187dcc0fc9bb8ccd2cec0cfaf9e6029
Marvel.Codes <marvellousn561@gmail.com>
2026-01-20 19:02:39 +0100

    removed nextjs caching in favour of gateway cache

--- src/app/[locale]/_actions.ts
+++ src/app/[locale]/_actions.ts
@@ -1,68 +1,20 @@
 import config from "@/config";
 import { formatNumber } from "@/modules/helpers/numFormatter";
 import axios, { AxiosError } from "axios";
-import { unstable_cache } from "next/cache";
 import {
 	type CMCTokenData,
 	cmcTokenDataSchema,
 	sophiaStatsSchema,
 } from "./_schema";
 
-const CACHE_DURATION_SECONDS_24_HR_IN_SECONDS = 24 * 60 * 60;
+const CACHE_DURATION_SECONDS_5_MIN_IN_SECONDS = 5 * 60;
 const SOPHIA_STATS_API_URL = `https://app.iqai.com/api/agents/stats?address=${config.sophiaAgentAddress}`;
 
-export const getIqStats = unstable_cache(
-	async () => {
-		try {
-			const cmcData = await fetchMarketCapData();
+export async function getIqStats() {
+	try {
+		const cmcData = await fetchMarketCapData();
 
-			if (!cmcData) {
-				return {
-					price: null,
-					mcap: null,
-					iqPriceChange: null,
-					formattedPriceChange: null,
-					iqMCapChange: null,
-					formattedMCapChange: null,
-				};
-			}
-
-			const price = formatNumber(cmcData.current_price, {
-				minDecimals: 4,
-				maxDecimals: 4,
-			});
-
-			const mcap = formatNumber(cmcData.market_cap, {
-				minDecimals: 2,
-				maxDecimals: 2,
-				compact: true,
-			});
-
-			const iqPriceChange = cmcData.price_change_percentage_24h;
-			const iqMCapChange = cmcData.market_cap_change_percentage_24h;
-
-			const formattedPriceChange = formatNumber(iqPriceChange, {
-				minDecimals: 2,
-				maxDecimals: 2,
-				signed: true,
-			});
-
-			const formattedMCapChange = formatNumber(iqMCapChange, {
-				minDecimals: 2,
-				maxDecimals: 2,
-				signed: true,
-			});
-
-			return {
-				price,
-				mcap,
-				iqPriceChange,
-				formattedPriceChange,
-				iqMCapChange,
-				formattedMCapChange,
-			};
-		} catch (error) {
-			console.error("Error in getIqDetails:", error);
+		if (!cmcData) {
 			return {
 				price: null,
 				mcap: null,
@@ -72,10 +24,58 @@ export const getIqStats = unstable_cache(
 				formattedMCapChange: null,
 			};
 		}
-	},
-	["iq-stats"],
-	{ revalidate: CACHE_DURATION_SECONDS_24_HR_IN_SECONDS },
-);
+
+		const price = formatNumber(cmcData.current_price, {
+			minDecimals: 4,
+			maxDecimals: 4,
+		});
+
+		const mcap = formatNumber(cmcData.market_cap, {
+			minDecimals: 2,
+			maxDecimals: 2,
+			compact: true,
+		});
+
+		const iqPriceChange = cmcData.price_change_percentage_24h;
+		const iqMCapChange = cmcData.market_cap_change_percentage_24h;
+
+		const formattedPriceChange = formatNumber(iqPriceChange, {
+			minDecimals: 2,
+			maxDecimals: 2,
+			signed: true,
+		});
+
+		const formattedMCapChange = formatNumber(iqMCapChange, {
+			minDecimals: 2,
+			maxDecimals: 2,
+			signed: true,
+		});
+
+		console.log("iqPriceChange", iqPriceChange);
+		console.log("formattedPriceChange", formattedPriceChange);
+		console.log("iqMCapChange", iqMCapChange);
+		console.log("formattedMCapChange", formattedMCapChange);
+
+		return {
+			price,
+			mcap,
+			iqPriceChange,
+			formattedPriceChange,
+			iqMCapChange,
+			formattedMCapChange,
+		};
+	} catch (error) {
+		console.error("Error in getIqDetails:", error);
+		return {
+			price: null,
+			mcap: null,
+			iqPriceChange: null,
+			formattedPriceChange: null,
+			iqMCapChange: null,
+			formattedMCapChange: null,
+		};
+	}
+}
 
 export async function getSophiaStats() {
 	try {
@@ -96,9 +96,16 @@ async function fetchMarketCapData(): Promise<CMCTokenData> {
 			},
 			params: {
 				url: "https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=everipedia",
-				cacheDuration: CACHE_DURATION_SECONDS_24_HR_IN_SECONDS,
+				cacheDuration: CACHE_DURATION_SECONDS_5_MIN_IN_SECONDS,
 			},
 		});
+
+		console.log("response.data", response.data);
+		console.log(
+			"response.data parsed",
+			cmcTokenDataSchema.parse(response.data[0]),
+		);
+
 		return cmcTokenDataSchema.parse(response.data[0]);
 	} catch (error) {
 		if (error instanceof AxiosError) {
